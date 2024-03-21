658867246c3f22fbc930aa9ea25c418a2104cbb0
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-03-21 15:09:35 -0400

    orbit: radius.py: mail_auth: remove hostname resolution cludge
    By creating a stream proxy in nginx that listens on loopback and
    proxies to the actual smtp/pop server we can have nginx resolve the
    hostname.

--- orbit/radius.py
+++ orbit/radius.py
@@ -23,9 +23,6 @@ min_per_ses = config.minutes_each_session_token_is_valid
 with open(config.doc_header) as header:
     html_header = header.read()
 
-import socket
-host_ip = socket.getaddrinfo('host.containers.internal',None,socket.AF_INET,socket.SOCK_STREAM,socket.IPPROTO_TCP)[0][4][0]
-
 # === utilities ===
 
 
@@ -420,7 +417,7 @@ def handle_mail_auth(rocket):
 
     rocket.headers += [('Auth-Status', 'OK'),
                        ('Auth-Port',    auth_port),
-                       ('Auth-Server', host_ip)]
+                       ('Auth-Server', '127.0.0.1')]
     return rocket.raw_respond(HTTPStatus.OK)
 
 
