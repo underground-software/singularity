782a333ac002a73892359d5f5c2c334d8b976866
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-03-24 14:13:38 -0400

    orbit: init db: users: provide default for LFX field
    Since we are moving away from this system of sharing resources
    a good place to start is to provide a default of false for lfx so
    that more places can avoid refering to it.

--- orbit/init-db.sql
+++ orbit/init-db.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
 	id integer primary key,
 	username string UNIQUE NOT NULL,
 	pwdhash string NOT NULL,
-	lfx boolean NOT NULL,
+	lfx boolean NOT NULL DEFAULT FALSE,
 	student_id integer);
 CREATE TABLE sessions (
         token string PRIMARY KEY,
