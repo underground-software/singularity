d45c5d1bb39a26b81e9f3777e07e3dd6954334b7
Joel Savitz <jsavitz@redhat.com>
2024-03-26 18:13:13 -0400

    test: simplify test-style.sh to remove bloat and pass shellcheck
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>

--- orbit/test-style.sh
+++ orbit/test-style.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-FLAKE8_FLAGS=""
-
-while getopts "v" FLAG; do
-        case $FLAG in
-        v)
-                FLAKE8_FLAGS="$FLAKE8_FLAGS "
-                ;;
-        esac
-done
-
-SOURCES=""
-scan() {
-	echo "[SCAN] ${1}"
-        flake8 ${FLAKE8_FLAGS} ${1}
-        RES=$?
-        if test "$RES" -ne "0"
-        then
-                exit 1
-        fi
-}
+scan() { echo "[SCAN] ${1}" ; flake8 "${1}" || exit 1 ; }
 
 scan radius.py
 scan config.py
