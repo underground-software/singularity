841a6c9cae12acc74812f325d98682bc6b1b6192
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-04-01 10:26:37 -0400

    test: move requirement for podman to warpdrive
    Since podman is never directly invoked in test.sh, it is
    clearer to delegate checking for it to the script that actually
    uses it (warpdrive)

--- orbit/warpdrive.sh
+++ orbit/warpdrive.sh
@@ -2,9 +2,14 @@
 #
 # the warp drive allows you to enter hyperspace
 
+require() { command -v "$1" > /dev/null || { echo "error: $1 command required yet absent" ; exit 1 ; } ; }
+
 cd "$(dirname "$0")"
 
 DOCKER=${DOCKER:-podman}
+
+require "${DOCKER}"
+
 CONTAINER=${CONTAINER:-singularity_orbit_1}
 
 cat <<EOF | $DOCKER exec -i "$CONTAINER" /bin/sh
