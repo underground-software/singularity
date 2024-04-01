1319042a99227e3a5a38ec13c3cb79b05166db84
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-04-01 10:47:11 -0400

    orbit: warpdrive: quote container variable reference
    Shellcheck SC2086 suggests doing this. It is unlikely that
    the container name could have a space (is it even possible?)
    but it is good practice nonetheless and tidier than having
    a nolint comment.

--- orbit/warpdrive.sh
+++ orbit/warpdrive.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 DOCKER=${DOCKER:-podman}
 CONTAINER=${CONTAINER:-singularity_orbit_1}
 
-cat <<EOF | $DOCKER exec -i $CONTAINER /bin/sh
+cat <<EOF | $DOCKER exec -i "$CONTAINER" /bin/sh
 . /radius-venv/bin/activate
 /orbit/hyperspace.py $@
 EOF
