ee59e7689ae75ca09a4820eaf6fed16398c2b2d0
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-04-01 10:20:34 -0400

    test: introduce script-lint.sh
    this script calls shellcheck on all the .sh files similar
    to test-style.sh from orbit.

---
+++ script-lint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+require() { command -v "$1" > /dev/null || { echo "error: $1 command required yet absent" ; exit 1 ; } ; }
+require shellcheck
+
+set -ex
+
+shellcheck script-lint.sh
+shellcheck test.sh
+shellcheck orbit/test-style.sh
