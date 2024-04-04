bbbf38640ecc392c8d56a33cd01dfd187f458779
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-04-04 13:44:03 -0400

    orbit: db: use strict tables in sqlite
    sqlite databases do not enforce strict types for columns by default
    instead it is possible to insert any sort of value into any column.
    Using the keyword "strict" after each table forces sqlite to error
    if it cannot losslessly convert a provided value into the type that
    a column specifies.

--- orbit/init-db.sql
+++ orbit/init-db.sql
@@ -4,14 +4,14 @@ CREATE TABLE users (
 	id integer primary key,
 	username text UNIQUE NOT NULL,
 	pwdhash text NOT NULL,
-	student_id text UNIQUE);
+	student_id text UNIQUE) STRICT;
 CREATE TABLE sessions (
         token text PRIMARY KEY,
         username text UNIQUE NOT NULL,
-        expiry real NOT NULL);
+        expiry real NOT NULL) STRICT;
 CREATE TABLE newusers (
 	registration_id integer primary key,
 	student_id text UNIQUE NOT NULL,
 	username text UNIQUE NOT NULL,
-	password text NOT NULL);
+	password text NOT NULL) STRICT;
 COMMIT;
