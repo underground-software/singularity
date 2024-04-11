6615baf24c1077cb9bdcff6b0c3c722d70007bd9
Joel Savitz <jsavitz@redhat.com>
2024-04-11 17:13:16 -0400

    watchtower: due: works
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>
    Upstream-commit: 04a2db0b15b123953d02e1ba04e64b74d313d7f2

--- watchtower/due.c
+++ watchtower/due.c
@@ -1,21 +1,23 @@
 #include <signal.h>
-#include <sys/signalfd.h>
 #include <limits.h>
 #include <inttypes.h>
 #include <sys/wait.h>
+#include <sys/timerfd.h>
 #include <time.h>
 #include <err.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdio.h>
 
-void handle_sigalrm(int signal_index) {
-	signal(SIGALRM, handle_sigalrm);
-}
+/* void handle_sigalrm(int signal_index) { */
+/* 	signal(SIGALRM, handle_sigalrm); */
+/* } */
 
 /*
  * argv must contain pairs of timestamps and paths to executables delimited by spaces
  * the timesamps must be monotonically increasing
  */
+
 int main (int argc, char **argv)
 {
 	/* // Avoid needing to reap children processes */
@@ -25,21 +27,33 @@ int main (int argc, char **argv)
 	/* child_act.sa_flags |= SA_NOCLDWAIT; */
 	/* if(0 > sigaction(SIGCHLD, &child_act, NULL)) */
 	/* 	err(1, "failed to set signal action for SIGCHLD (this is a bug)"); */
-	signal(SIGALRM, handle_sigalrm);
-	sigset_t mask;
-	if (sigfillset(&mask))
-		err(1, "sigfillset");
-	int sigfd = signalfd(-1, &mask, SFD_CLOEXEC);
+	//signal(SIGALRM, handle_sigalrm);
+	/* sigset_t mask; */
+	/* int sigfd; */
+	/* if (sigemptyset(&mask) == -1) */
+	/* 	err(1, "sigemptyset"); */
+	/* if (sigaddset(&mask, SIGALRM) == -1) */
+	/* 	err(1, "sigaddset"); */
+	/* if (sigprocmask(SIG_BLOCK, &mask, NULL) == -1) */
+	/* 	err(1, "sigprocmask"); */
+
+       	/* if ((sigfd = signalfd(-1, &mask, SFD_CLOEXEC)) == -1) */
+		/* err(1, "signalfd"); */
+
+	int timerfd;
+	if (0 > (timerfd = timerfd_create(CLOCK_REALTIME, TFD_CLOEXEC)))
+		err(1, "timerfd_create");
 
 	for (char * next; next = *++argv;) {
 		char * endptr;
-		intmax_t timestamp = strtoimax(next, &endptr, 10);
-		if (endptr == next || *endptr != '\0')
+		intmax_t parsed_timestamp = strtoimax(next, &endptr, 10);
+		if (endptr == next || *endptr != '\0' || parsed_timestamp != (intmax_t)(time_t)parsed_timestamp)
 			errx(1, "failed to parse argv entry \"%s\"", next);
+		time_t timestamp = (time_t)parsed_timestamp;
 		// get executable path
 		next = *++argv;
 		for (;;) {
-			intmax_t now = (intmax_t)time(NULL);
+			time_t now = time(NULL);
 			// once the due date is in the past, exec
 			if (timestamp < now) {
 				pid_t pid;
@@ -54,21 +68,17 @@ int main (int argc, char **argv)
 						err(1, "waitpid failed");
 					if (!WIFEXITED(childret) || WEXITSTATUS(childret))
 						warnx("child (%s) exited abnormally with status=%d", next, childret);
+					goto next_entry;
 				}
 			}
-			if (timestamp - now > UINT_MAX)
-				errx(1, "due date more than 2**32 seconds in the future");
-			// now safe to downcast intmax_t difference to unsigned
-			unsigned timetowait = (unsigned)(timestamp - now);
-			if (alarm(timetowait))
-				errx(1, "bug! pending alarm detected (how?)");
 
-			struct signalfd_siginfo info;
-			if (read(sigfd, &info, sizeof info) != sizeof info)
-				err(1, "failed to read signal info");
-			if (info.ssi_signo != SIGALRM)
-				errx(1, "caught unexepcted signal %d", info.ssi_signo);
+			if (0 > timerfd_settime(timerfd, TFD_TIMER_ABSTIME | TFD_TIMER_CANCEL_ON_SET,
+					&(struct itimerspec){.it_value.tv_sec = (time_t)timestamp }, NULL))
+				err(1, "timerfd_settime");
+
+			read(timerfd, &(uint64_t){}, sizeof (uint64_t));
 		}
+next_entry:
 	}
 
 	return 0;
