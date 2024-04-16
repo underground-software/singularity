35e4735642567c5dcab91c3857e985e784706478
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-04-16 18:08:33 -0400

    watchtower: watch: working version
    added a makefile that compiles both programs
    
    Upstream-commit: 6723f19092bf2590716ed4099bf7b41e4c85e6e0

--- watchtower/watch.c
+++ watchtower/watch.c
@@ -22,10 +22,18 @@ int main (int, char **argv)
 	// skip the program name
 	++argv;
 
+	int last_watch_desc = -1;
 	// for each pair of (watched directory, executable path) in argv
-	for (char *dir, *exe, **argv_iter = argv; (dir = argv_iter[0]) && (exe = argv_iter[1]); argv_iter += 2) {
-		argv_iter[0] = (intptr_t)inotify_add_watch(inotifyfd, dir, IN_CREATED);
+	for (char *dir, *exe; (dir = argv[0]) && (exe = argv[1]); argv += 2) {
+		int watch_desc = inotify_add_watch(inotifyfd, dir, IN_MASK_CREATE | IN_ONLYDIR | IN_CREATE);
+		if(0 > watch_desc)
+			err(1, "failed to create watch for directory: '%s'", dir);
+		if(-1 != last_watch_desc && watch_desc != last_watch_desc + 1)
+			errx(1, "watch descriptors from the kernel were not sequential. (BUG!!!)");
+		last_watch_desc = watch_desc;
 	}
+	if(last_watch_desc == -1)
+		errx(1, "no directories provided");
 
 	FILE * inotify_file = fdopen(inotifyfd, "r");
 
@@ -42,19 +50,21 @@ int main (int, char **argv)
 		if (event->len != fread(event->name, sizeof(char), (size_t)event->len, inotify_file))
 			err(1, "fread");
 
-		for (char *dir, *exe, **argv_iter = argv; (dir = argv_iter[0]) && (exe = argv_iter[1]); argv_iter += 2) {
-			// run the program as a child process and wait for it to finish
-			pid_t pid = fork();
-			if (0 > pid)
-				err(1, "fork failed");
-			if (!pid && 0 > execl(exe, exe, (char *)NULL))
-				err(1, "failed to exec '%s'", exe);
-			int childret;
-			if (waitpid(pid, &childret, 0) == -1)
-				err(1, "waitpid failed");
-			if (!WIFEXITED(childret) || WEXITSTATUS(childret))
-				warnx("child (%s) exited abnormally with status=%d", exe, childret);
-		}
+		//argv has been incremented to point to the end of the array instead of the beginning
+		char **argv_pair = &argv[2 * (event->wd - last_watch_desc)];
+
+		char *dir = argv_pair[-2];
+		char *exe = argv_pair[-1];
+
+		pid_t pid = fork();
+		if (0 > pid)
+			err(1, "fork failed");
+		if (!pid && 0 > execl(exe, exe, dir, event->name, (char *)NULL))
+			err(1, "failed to exec '%s'", exe);
+		int childret;
+		if (waitpid(pid, &childret, 0) == -1)
+			err(1, "waitpid failed");
+		if (!WIFEXITED(childret) || WEXITSTATUS(childret))
+			warnx("child (%s) exited abnormally with status=%d", exe, childret);
 	}
-	return 0;
 }
