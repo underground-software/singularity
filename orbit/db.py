997a2d753749e9528151ed44c10cf3f48dba4126
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-04-25 21:47:40 -0400

    orbit: db: generate schema from peewee orm classes
    instead of manually specifying schemata in init-db.sql, we can describe
    the fields and their properties as members of classes within a hierarchy
    of classes inheriting from peewee orm's base model.
    
    Obviously peewee orm can do a lot more than generate schemata, but this
    is a nice (and fully backwards compatible) start on the work towards
    fully embracing peewee for all database operations.
    
    The existing queries continue to work because the schemata generated are
    fully compatible with the existing ones (albeit not identical, so the
    binary blob needed updating).
    
    This means that the existing db code can be slowly phased out in the next
    commits and orbit will remain fully functional throughout.

--- orbit/db.py
+++ orbit/db.py
@@ -1,10 +1,51 @@
+#!/usr/bin/env python3
 import sqlite3
+import peewee
 import config
 # nickname  table name
 # USR => users
 # SES => sessions
 # REG => newusers
 import sys
+
+DB = peewee.SqliteDatabase(config.database)
+
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = DB
+        strict_tables = True
+
+
+class User(BaseModel):
+    username = peewee.TextField(unique=True)
+    pwdhash = peewee.TextField()
+    student_id = peewee.TextField(unique=True, null=True)
+
+    class Meta:
+        table_name = 'users'
+
+
+class Session(BaseModel):
+    token = peewee.TextField(primary_key=True)
+    username = peewee.TextField(unique=True)
+    expiry = peewee.FloatField()
+
+    class Meta:
+        table_name = 'sessions'
+
+
+class Registration(BaseModel):
+    student_id = peewee.TextField(unique=True)
+    username = peewee.TextField(unique=True)
+    password = peewee.TextField()
+
+    class Meta:
+        table_name = 'newusers'
+
+
+if __name__ == '__main__':
+    DB.create_tables(BaseModel.__subclasses__())
 
 
 def _do(cmd, reps=(), set_=False, get_=False):
