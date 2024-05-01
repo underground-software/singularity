a852260b79402b9cc9ead39d260ffb47cfbd3bc8
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-05-01 17:10:19 -0400

    orbit: Containerfile: set workdir and prefer relative paths
    There are lots of absolute paths scattered around the orbit codebase.
    By just setting the workdir to the location of the orbit source folder
    within the Containerfile most of those paths can be converted into
    relative paths which makes moving the orbit source code easier.

--- orbit/config.py
+++ orbit/config.py
@@ -1,9 +1,8 @@
 version_info = '${orbit_version_info}'
 
 # read these documents from a filesystem path
-orbit_root = '/orbit'
-doc_root = f'{orbit_root}/docs'
-doc_header = f'{orbit_root}/header.html'
+doc_root = './docs'
+doc_header = './header.html'
 database = '/var/orbit/orbit.db'
 
 # duration of authentication token validity period
