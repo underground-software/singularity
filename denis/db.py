9259f6735140d1dd83bd353e0a92fb1a292792e3
Joel Savitz <jsavitz@redhat.com>
2024-05-07 16:35:21 -0400

    denis: automate denis using container technology
    We can rebuild him. The future is now.
    
    The singularity is here.
    
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>

---
+++ denis/db.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import peewee
+
+DB = peewee.SqliteDatabase("/var/lib/denis/grades.db")
+
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = DB
+        strict_tables = True
+
+
+class Submission(BaseModel):
+    submission_id = peewee.TextField(unique=True)
+    assignment = peewee.TextField()
+    timestamp = peewee.IntegerField()
+    user = peewee.TextField()
+    status = peewee.TextField()
+
+
+if __name__ == '__main__':
+    DB.create_tables(BaseModel.__subclasses__())
