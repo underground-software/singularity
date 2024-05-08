a8d33a728ed1fa4e0c47526a6eac9913af5d458a
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-05-08 17:14:25 -0400

    watchtower: watch: simplify to support only one dir/program
    We don't need to watch more than on directory. If we really wanted
    to do that, we could just spawn more than one of these programs.
    
    Upstream-commit: e4e97688d8cb3bb1abe5af7ee39b23c0386c8082

--- watchtower/watch.c
+++ watchtower/watch.c
@@ -23,42 +23,23 @@ static struct inotify_event *get_event(void)
 	return evt;
 }
 
-/*
- * argv must contain pairs of paths to directories and paths to executables delimited by spaces
- */
-
-int main (int, char **argv)
+int main(int argc, char **argv)
 {
 	if (0 > (inotifyfd = inotify_init1(IN_CLOEXEC)))
 		err(1, "inotify_init1");
 
-	// skip the program name
-	++argv;
+	if (argc != 3)
+		errx(1, "Usage: %s directory script", argv[0]);
+	char *dir = argv[1], *exe = argv[2];
 
-	int last_watch_desc = -1;
-	// for each pair of (watched directory, executable path) in argv
-	for (char *dir, *exe; (dir = argv[0]) && (exe = argv[1]); argv += 2) {
-		int watch_desc = inotify_add_watch(inotifyfd, dir, IN_MASK_CREATE | IN_ONLYDIR | IN_CREATE);
-		if(0 > watch_desc)
-			err(1, "failed to create watch for directory: '%s'", dir);
-		if(-1 != last_watch_desc && watch_desc != last_watch_desc + 1)
-			errx(1, "watch descriptors from the kernel were not sequential. (BUG!!!)");
-		last_watch_desc = watch_desc;
-	}
-	if(last_watch_desc == -1)
-		errx(1, "no directories provided");
-
+	int watch_desc = inotify_add_watch(inotifyfd, dir, IN_MASK_CREATE | IN_ONLYDIR | IN_CREATE);
+	if (0 > watch_desc)
+		err(1, "failed to create watch for directory: '%s'", dir);
 
 	for (;;) {
 		struct inotify_event *event = get_event();
 		if (!event)
 			err(1, "get event");
-
-		//argv has been incremented to point to the end of the array instead of the beginning
-		char **argv_pair = &argv[2 * (event->wd - last_watch_desc)];
-
-		char *dir = argv_pair[-2];
-		char *exe = argv_pair[-1];
 
 		pid_t pid = fork();
 		if (0 > pid)
