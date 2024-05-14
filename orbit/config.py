4404e295e6e7af47157d61571fa05bf17757d4a6
Joel Savitz <jsavitz@redhat.com>
2024-05-13 20:18:32 -0400

    orbit: rename /var/orbit mountpoint to /var/lib/orbit
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>

--- orbit/config.py
+++ orbit/config.py
@@ -3,7 +3,7 @@ version_info = '${orbit_version_info}'
 # read these documents from a filesystem path
 doc_root = './docs'
 doc_header = './header.html'
-database = '/var/orbit/orbit.db'
+database = '/var/lib/orbit/orbit.db'
 
 # duration of authentication token validity period
 minutes_each_session_token_is_valid = 180
