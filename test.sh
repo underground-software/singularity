6efa942db841755259355850e446b85e32d42a38
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-05-17 14:04:40 -0400

    test: remove cleanup and restore code
    The cleanup and restore code doesn't even work so the tests are not
    currently idempotent anyways. This code is basically just dead weight
    that makes the tests harder to maintain.
    
    After this patch, it is a precondition for starting the test script
    that the containers are started with fresh volumes (i.e. that you
    just did a podman volume prune command before bringing the containers up),
    and after the tests run, the volumes are left in and unspecified state.
    
    You really shouldn't be running the tests on an instance with important/
    production data in the volumes, but if you really want to, you should make
    a meta script that uses the existing backup and restore scripts to save
    the volumes brings the containers down, prunes the volumes, brings them
    back up, runs the tests brings them down and then restores the volumes.

--- test.sh
+++ test.sh
@@ -10,19 +10,6 @@
 #   of the furthest right failing command or zero if no command failed (o pipefail)
 set -exuo pipefail
 
-# This function will push an action to a stack of items to be done on script exit
-# in the reverse order that they are passed to this function
-trap ":" EXIT
-add_cleanup() {
-	trap -- "$(
-		printf '%s\n' "$1"
-		# get stack is invoked in eval but shellcheck cannot tell since it is indirect
-		# shellcheck disable=SC2317
-		get_stack() { printf '%s\n' "$3"; }
-		eval "get_stack $(trap -p EXIT)"
-	)" EXIT
-}
-
 DOCKER=${DOCKER:-podman}
 
 require() { command -v "$1" > /dev/null || { echo "error: $1 command required yet absent" ; exit 1 ; } ; }
@@ -57,67 +44,12 @@ SINGULARITY_HOSTNAME=${SINGULARITY_HOSTNAME:-"${HOSTNAME_FROM_DOTENV}"}
 
 ${DOCKER} cp singularity_nginx_1:/etc/ssl/nginx/fullchain.pem test/ca_cert.pem
 
-nuke_mail() {
-	${DOCKER} run --rm -v singularity_email:/var/lib/email alpine:3.19 sh -c 'rm -f /var/lib/email/mail/* /var/lib/email/logs/*'
-}
-
-# Save original contents of mail volume
-${DOCKER} volume export singularity_email > test/email_orig.tar
-
-#start with no mail and restore any saved messages once the test completes
-nuke_mail
-add_cleanup "${DOCKER} volume import singularity_email test/email_orig.tar"
-
 CURL_OPTS=( \
 --verbose \
 --cacert test/ca_cert.pem \
 --fail \
 --no-progress-meter \
 )
-
-# Save original contents of orbit db volume
-${DOCKER} volume export singularity_orbit-db > test/orbit_orig.tar
-
-# Import an empty orbit db with no users or sessions
-xxd -r <<- 'EOF' | gunzip | ${DOCKER} volume import singularity_orbit-db -
-00000000: 1f8b 0800 0000 0000 0003 edda 4d8f d240  ............M..@
-00000010: 18c0 f176 79a9 9af0 7221 3d70 9970 51a2  ...vy...r!=p.pQ.
-00000020: bb01 25ae 37c5 b531 44ec ba6c 49d8 8ba4  ..%.7..1D..lI...
-00000030: a4dd 6ce3 2eac 6d89 eb11 13bf 88df c6ab  ..l...m.........
-00000040: dfc1 efe0 d119 282f 4b34 d90b 6af0 ff4b  ......(/K4..j..K
-00000050: 3a33 cf74 78a6 0fe9 6502 a370 10c4 7bde  :3.tx...e..p..{.
-00000060: 40db a09a f4b8 d198 f6f5 b5be 56db 574d  @...........V.WM
-00000070: 32ae 25e3 fd46 fda1 266a 9b7c a8b9 7114  2.%..F..&j.|..q.
-00000080: bba1 dcf2 4fec f50f 3a3e 6a07 b12f 4e47  ....O...:>j../NG
-00000090: e185 1b8b 475a 51d3 75ed 9910 f256 465e  ....GZQ.u....VF^
-000000a0: c6ca 5215 a757 62fd 06e9 33da 5ef8 2557  ..R..Wb...3.^.%W
-000000b0: f8a1 6573 9fb5 c293 c2d7 fcb7 fcd3 dc77  ..es...........w
-000000c0: 1900 0000 00c0 ffe8 6dd6 30ef 97f4 c9ed  ........m.0.....
-000000d0: 60e8 f957 e3c8 0ffb 513c f6fc 61dc 0f3c  `..W....Q<..a..<
-000000e0: 151a 071d abe9 58a2 6bb7 8eba 9668 d92f  ......X.k....h./
-000000f0: ac9e a8ac 2dac 8843 7b36 5911 f72a 2bf3  ....-..C{6Y..*+.
-00000100: d55e c630 efca fcfa 32bf 6a86 ee85 affa  .^.0....2.j.....
-00000110: ecef b3cf 975d cfbd 98ad 4e8a 69c3 2c95  .....]....N.i.,.
-00000120: f44f a9d8 1d9c 4fb3 a92b 9364 749a cfdb  .O....O..+.dt...
-00000130: d6f2 73ea 195b b663 bdb4 3ac2 3e74 84dd  ..s..[.c..:.>t..
-00000140: 6db7 c59b 4eeb 75b3 7322 5e59 270f c432  m...N.u.s"^Y'..2
-00000150: b570 ac9e b358 25ef 5c7e f0ce dce8 6c76  .p...X%.\~....lv
-00000160: 43c6 ab85 aba9 aa38 763a ad03 c74b 19e6  C......8v:...K..
-00000170: 6e59 9fe4 a6c5 467e 1405 a3e1 a290 244e  nY....F~......$N
-00000180: ffb2 e4f5 c5b3 aa93 d9b5 c2f3 ba61 96e5  .............a..
-00000190: 36d1 b4f0 644d d2ed 5c2f 7f25 413c 7ae7  6...dM..\/.%A<z.
-000001a0: 0fd7 6abb f137 e05f 5d06 e1c7 8a90 c9db  ..j..7._].......
-000001b0: 8bf9 79d9 bb3b 59b3 59d6 b559 d5ef cfe5  ..y..;Y.Y..Y....
-000001c0: a9be ef8e e3d1 34ee cf2b ab27 8394 7ceb  ......4..+.'..|.
-000001d0: 6ea9 572f a79a e25f 7dfb 0100 0000 00c0  n.W/..._}.......
-000001e0: 86dd 510d e77f 0000 0000 00b6 1ae7 7f00  ..Q.............
-000001f0: 0000 0000 b61f ffff 0700 0000 0060 fbf1  .............`..
-00000200: fb3f 0000 0000 00db 8ff3 3f00 0000 0000  .?........?.....
-00000210: 0000 6ca7 9f2d 67a0 8900 7800 00         ..l..-g...x..
-EOF
-
-# Restore the old orbit db after testing completes
-add_cleanup "${DOCKER} volume import singularity_orbit-db test/orbit_orig.tar"
 
 # Check that registration fails before user creation
 curl --url "https://$SINGULARITY_HOSTNAME/register" \
@@ -137,8 +69,6 @@ curl --url "https://$SINGULARITY_HOSTNAME/login" \
 
 # Check that we can create a user
 orbit/warpdrive.sh -u user -i 1234 -n
-
-add_cleanup "orbit/warpdrive.sh -u user -w"
 
 # Check that registration fails with incorrect student id
 curl --url "https://$SINGULARITY_HOSTNAME/register" \
@@ -209,8 +139,6 @@ EOF
 ) | tee test/smtp_send_email \
   | diff <(printf "") /dev/stdin
 
-# Remove the email we just sent
-add_cleanup nuke_mail
 
 # Check that the user can get the most recent message sent to the server
 curl --url "pop3s://$SINGULARITY_HOSTNAME/1" \
