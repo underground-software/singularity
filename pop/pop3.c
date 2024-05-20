underground-software/singularity

commit 710b05132a91c79cabf790ac6b1c4192686a830d

pop: switch to caching email data in journal file
The current model for loading email in the pop server is very inefficient:
we have to open the mail directory iterate over and stat every file in the
directory to collect metadata about the emails (regardless of how many the
user actually wants to access) and then throw all the data away when the
client disconnects.

This was essentially necessary to implement the unix permissions based
system of access control, but as that is not even working / being used
anymore, it is just a massive waste of time.

As an alternative, the array of emails structures that would be built by
the pop program by reading the list of mail can be built by a separate
program and stored in a file that is `mmap`ed into memory by the pop
program on startup. This offers an essentially O(1) vs O(n) speed
improvement on the existing startup procedure where n is the number of
emails in the maildrop with the only downside that this journal file must
be updated periodically to give access to new emails.

This however is not a serious issue, because email is read more often than
sent (this is especially true in a mailing list where every email sent is
read by as many people as are on the list), and the need to update the
file can be used as an opportunity to inject access control logic and hold
back email from being visible until appropriate opening the door for a
more flexible replacement to the prior permissions scheme in addition to
the substantial performance upside.

The new program has two modes of operation. It can create a new empty mail
journal file which is used during building the container to initialize the
journal, and a second mode where a folder containing email and a temporary
file name is provided and the program reads all the mail and creates a new
journal file with all the data in the temporary location before swapping
it in to replace the real journal atomically once everything is settled.

pop/pop3.c
@@ -1,7 +1,5 @@
 #include <ctype.h>
-#include <dirent.h>
 #include <err.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
 #include <limits.h>
@@ -10,8 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/sendfile.h>
-#include <sys/stat.h>
 #include <sys/xattr.h>
 #include <time.h>
 #include <uchar.h>
@@ -147,44 +145,24 @@ static bool check_credentials(size_t u_size, const char *username, size_t p_size
 static struct email *maildrop;
 static size_t num_emails;
 
-static void load_emails(void)
+static void load_emails(int journal_fd)
 {
-	DIR *dir = opendir(".");
-	if(!dir)
-		err(1, "Unable to open mail directory");
-	size_t capacity = 16;
-	maildrop = malloc(capacity * sizeof *maildrop);
-	errno = 0;
-	for(struct dirent *ptr = readdir(dir); NULL != ptr; errno = 0, ptr = readdir(dir))
-	{
-		int fd = open(ptr->d_name, O_RDONLY | O_NOFOLLOW);
-		if(0 > fd)
-			err(1, "unable to open file \"%s\"", ptr->d_name);
-		struct stat statbuf;
-		if(0 > fstat(fd, &statbuf))
-			err(1, "unable to stat file \"%s\"", ptr->d_name);
-		//skip non regular files (e.g. symlinks, directories)
-		if(!S_ISREG(statbuf.st_mode))
-			continue;
-		struct email email =
-		{
-			.size = statbuf.st_size,
-			.active = true,
-		};
-		if(sizeof email.top_limit != fgetxattr(fd, "user.top_limit", &email.top_limit, sizeof email.top_limit))
-			err(1, "unable to read end of headers marker from email \"%s\"", ptr->d_name);
-		close(fd);
-		size_t size = strlen(ptr->d_name);
-		if(size >= sizeof email.name)
-			errx(1, "filename of email %s is too long", ptr->d_name);
-		memcpy(email.name, ptr->d_name, size + 1);
-		if(num_emails >= capacity && NULL == (maildrop = realloc(maildrop, (capacity *= 2) * sizeof *maildrop))) //this leaks the old pointer if it fails, but since we exit immediately, this isn't a bit deal
-			err(1, "unable to resize maildrop");
-		maildrop[num_emails++] = email;
-	}
-	if(errno)
-		err(1, "Unable to read from directory");
-	closedir(dir);
+	off_t limit;
+	ssize_t ret = fgetxattr(journal_fd, "user.data_end", &limit, sizeof limit);
+	if(sizeof limit != ret)
+		err(1, "unable to read journal size from journal file");
+	if(0 > limit)
+		errx(1, "invalid journal size: negative");
+	size_t maildrop_size = (size_t)limit;
+	if(maildrop_size % sizeof(struct email))
+		errx(1, "invalid journal size: not divisible by size of email struct");
+	num_emails = maildrop_size / sizeof(struct email);
+	//mmap will not accept a size of zero, so we need to check, but if size is zero, the
+	//default value of NULL for maildrop is perfectly adequate since no code should access it.
+	if(num_emails)
+		maildrop = mmap(NULL, maildrop_size, PROT_READ, MAP_PRIVATE, journal_fd, 0);
+	if(MAP_FAILED == maildrop)
+		err(1, "unable to map journal file");
 }
 
 static bool pending_deletes(void)
@@ -218,11 +196,14 @@ int main(int argc, char **argv)
 	if(0 > fcntl(STDOUT_FILENO, F_SETFL, flags))
 		err(1, "unable to set flags for stdout");
 
-	if(argc != 2)
-		errx(1, "Usage: %s <mail directory>", argv[0]);
+	if(argc != 3)
+		errx(1, "Usage: %s <mail directory> <journal file>", argv[0]);
+	int journal_fd = open(argv[2], O_RDONLY);
+	if(0 > journal_fd)
+		err(1, "Unable to open journal file \"%s\"", argv[1]);
 	if(chdir(argv[1]))
 		errx(1, "Unable to change directory to mail folder \"%s\"", argv[1]);
-	load_emails();
+	load_emails(journal_fd);
 	SEND("+OK POP3 server ready");
 	for(enum state state = START; state != QUIT;)
 	{

.gitignore
@@ -1,4 +1,5 @@
 /pop/pop3
+/pop/init_journal
 /smtp/smtp
 /tcp_server/tcp_server
 /test

backup/volumes_list
@@ -1,6 +1,7 @@
 VOLUMES='
 singularity_email-mail
 singularity_email-logs
+singularity_email-journal
 singularity_orbit-db
 singularity_ssl-certs
 singularity_submatrix-data

container-compose.yml
@@ -90,6 +90,10 @@ services:
         source: email-mail
         target: /var/lib/email/mail
         read_only: true
+      - type: volume
+        source: email-journal
+        target: /var/lib/email/journal
+        read_only: false
     depends_on:
       - smtp
     networks:
@@ -143,6 +147,7 @@ volumes:
   ssl-certs:
   email-mail:
   email-logs:
+  email-journal:
   orbit-db:
   grades-db:
   submatrix-data:

pop/.dockerignore
@@ -1,1 +1,2 @@
 pop3
+init_journal

pop/Containerfile
@@ -12,15 +12,22 @@ COPY . /pop
 
 RUN make -C /pop CC='clang -static'
 
+RUN mkdir -p /var/lib/email/journal && \
+	/pop/init_journal /var/lib/email/journal/journal && \
+	chown -R 100:100 /var/lib/email/journal && \
+	:
+
 FROM scratch as pop
 
 VOLUME /var/lib/email
 
 COPY --from=build /tcp_server/tcp_server /usr/local/bin/tcp_server
 COPY --from=build /pop/pop3 /usr/local/bin/pop3
+COPY --from=build /pop/init_journal /usr/local/bin/init_journal
+COPY --from=build /var/lib/email/ /var/lib/email/
 
 USER 100:100
 
 ARG LISTEN_PORT=995
 EXPOSE ${LISTEN_PORT}
-ENTRYPOINT ["/usr/local/bin/tcp_server", "/usr/local/bin/pop3", "pop3", "/var/lib/email/mail"]
+ENTRYPOINT ["/usr/local/bin/tcp_server", "/usr/local/bin/pop3", "pop3", "/var/lib/email/mail", "/var/lib/email/journal/journal"]

pop/Makefile
@@ -8,10 +8,13 @@ endif
 
 .PHONY: all clean
 
-all: pop3
+all: pop3 init_journal
 
 pop3: pop3.c types.h
 	$(CC) $(CFLAGS) -o $@ $<
 
+init_journal: init_journal.c types.h
+	$(CC) $(CFLAGS) -o $@ $<
+
 clean:
-	-rm pop3
+	-rm pop3 init_journal

pop/init_journal.c
@@ -1,0 +1,116 @@
+#include <dirent.h>
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/xattr.h>
+#include <unistd.h>
+
+#include "types.h"
+
+//despite being in linux since 3.15 (2014) and glibc since 2.28 (2018) musl still
+//does not have support for the renameat2 system call (though at time of writing,
+//it is hopefully coming soon https://www.openwall.com/lists/musl/2024/05/07/7)
+#ifndef RENAME_EXCHANGE
+#include <sys/syscall.h>
+#define RENAME_EXCHANGE  (1 << 1)
+static int renameat2(int oldfd, const char *old, int newfd, const char *new, unsigned flags)
+{
+	return (int)syscall(SYS_renameat2, oldfd, old, newfd, new, flags);
+}
+#endif
+
+static void write_with_retry(int fd, void *data, size_t size)
+{
+	char *data_p = data;
+	size_t off = 0;
+	do
+	{
+		ssize_t ret = write(fd, data_p + off, size - off);
+		if(ret < 0)
+			err(1, "unable to write data");
+		off += (size_t)ret;
+	}
+	while(off < size);
+}
+
+static void load_emails(int journal_fd, char *path)
+{
+	int mail_dir = openat(AT_FDCWD, path, O_RDONLY | O_DIRECTORY);
+	if(0 > mail_dir)
+		err(1, "unable to open mail directory \"%s\"", path);
+	DIR *dir = fdopendir(mail_dir);
+	if(!dir)
+		err(1, "Unable to open directory stream");
+	errno = 0;
+	for(struct dirent *ptr = readdir(dir); NULL != ptr; errno = 0, ptr = readdir(dir))
+	{
+		int fd = openat(mail_dir, ptr->d_name, O_RDONLY | O_NOFOLLOW);
+		if(0 > fd)
+			err(1, "unable to open file \"%s\"", ptr->d_name);
+		struct stat statbuf;
+		if(0 > fstat(fd, &statbuf))
+			err(1, "unable to stat file \"%s\"", ptr->d_name);
+		//skip non regular files (e.g. symlinks, directories)
+		if(!S_ISREG(statbuf.st_mode))
+			continue;
+		struct email email =
+		{
+			.size = statbuf.st_size,
+			.active = true,
+		};
+		if(sizeof email.top_limit != fgetxattr(fd, "user.top_limit", &email.top_limit, sizeof email.top_limit))
+			err(1, "unable to read end of headers marker from email \"%s\"", ptr->d_name);
+		close(fd);
+		size_t size = strlen(ptr->d_name);
+		if(size >= sizeof email.name)
+			errx(1, "filename of email %s is too long", ptr->d_name);
+		memcpy(email.name, ptr->d_name, size + 1);
+		write_with_retry(journal_fd, &email, sizeof email);
+	}
+	if(errno)
+		err(1, "Unable to read from directory");
+	closedir(dir);
+}
+
+
+int main(int argc, char **argv)
+{
+	char *journal_file, *temp_file, *email_folder=NULL, *new_file;
+	switch(argc)
+	{
+	case 2:
+		new_file = argv[1];
+		break;
+	case 4:
+		journal_file = argv[1];
+		new_file = temp_file = argv[2];
+		email_folder = argv[3];
+		break;
+	default:
+		errx(1, "Usage: %s [journal file] ([temp file name] [folder with emails])", argv[0]);
+	}
+
+	int new_fd = openat(AT_FDCWD, new_file, O_CREAT | O_EXCL | O_WRONLY, 0600);
+	if(0 > new_fd)
+		err(1, "Unable to create file \"%s\"", new_file);
+	if(email_folder)
+		load_emails(new_fd, email_folder);
+	off_t f_pos = lseek(new_fd, 0, SEEK_CUR);
+	if(fsetxattr(new_fd, "user.data_end", &f_pos, sizeof f_pos, 0))
+		err(1, "unable to write journal file size to journal file");
+	if(fdatasync(new_fd))
+		err(1, "unable to sync data to disk");
+	if(close(new_fd))
+		err(1, "error occured while closing journal file");
+	if(!email_folder)
+		return 0;
+	if(renameat2(AT_FDCWD, journal_file, AT_FDCWD, temp_file, RENAME_EXCHANGE))
+		err(1, "unable to update replace journal file we new data");
+	if(unlink(temp_file))
+		err(1, "unable to remove temporary file");
+	return 0;
+}

commit 59133a0b03ca8524c8055e013329580b004d633e

pop: fix segfault on RSET or DELE command
Even though the underlying file should be opened O_RDONLY we still want to
allow PROT_WRITE on the mapping of it so that the active field of the mail
can be updated in response to DELE or RSET commands.

Fixes: 710b051 ("pop: switch to caching email data in journal file")

pop/pop3.c
@@ -198,7 +198,7 @@ static void load_emails(int journal_fd, size_t username_size, char *username)
 	//mmap will not accept a size of zero, so we need to check, but if size is zero, the
 	//default value of NULL for maildrop is perfectly adequate since no code should access it.
 	if(num_emails)
-		maildrop = mmap(NULL, maildrop_size, PROT_READ, MAP_PRIVATE, journal_fd, 0);
+		maildrop = mmap(NULL, maildrop_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, journal_fd, 0);
 	if(MAP_FAILED == maildrop)
 		err(1, "unable to map journal file");
 }