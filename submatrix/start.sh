underground-software/singularity

commit 175e5a5bfec1f65aba9108129181f5d214d003d4

submatrix: initial version
Create a new container for the synapse matrix server. The name submatrix
is to indicate that this instance is subordinate to our mothership
matrix server and cannot federate with any other non-mothership server.
Users authenticate with the Orbit ID credentials for matrix just like
email and the dashboard. Like the other containers, submatrix logs output
to stderr. In addition, this patch adds curl-based matrix login tests.

Signed-off-by: Joel Savitz <jsavitz@redhat.com>

submatrix/Containerfile
@@ -1,0 +1,20 @@
+FROM alpine:latest as submatrix
+
+RUN apk add synapse envsubst py3-curl
+
+COPY homeserver.yaml.template /etc/synapse/homeserver.yaml.template
+
+COPY homeserver.log.config /etc/synapse/homeserver.log.config
+
+COPY orbit_auth.py /usr/lib/python3.11/site-packages/orbit_auth.py
+
+ARG MATRIX_HOSTNAME=localhost
+
+RUN envsubst '$MATRIX_HOSTNAME' < /etc/synapse/homeserver.yaml.template > /etc/synapse/homeserver.yaml
+
+VOLUME /var/synapse
+
+USER 100:100
+
+CMD /usr/bin/synapse_homeserver -c /etc/synapse/homeserver.yaml
+

container-compose.yml
@@ -31,6 +31,7 @@ services:
       - orbit
       - smtp
       - pop
+      - submatrix
   orbit:
     build:
       context: orbit
@@ -84,11 +85,29 @@ services:
       - smtp
     networks:
       - pop
+  submatrix:
+    build:
+      context: submatrix
+      dockerfile: Containerfile
+      args:
+        MATRIX_HOSTNAME: ${SINGULARITY_HOSTNAME}
+    volumes:
+      - type: volume
+        source: submatrix-data
+        target: /var/synapse
+        read_only: false
+    depends_on:
+      - orbit 
+    networks:
+      - submatrix
+      - orbit
 networks:
   orbit:
   smtp:
   pop:
+  submatrix:
 volumes:
   ssl-certs:
   email:
   orbit-db:
+  submatrix-data:

nginx_snippets/server_https/04-matrix-paths.conf
@@ -1,0 +1,20 @@
+location /.well-known/matrix/client {
+	default_type application/json;
+	add_header Access-Control-Allow-Origin *;
+	return 200 '{"m.homeserver": {"base_url": "https://$server_name:443"}}';
+}
+
+location ~ ^(/_matrix|/_synapse/client) {
+
+        proxy_pass http://submatrix:8008;
+        proxy_set_header X-Forwarded-For $remote_addr;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host $host;
+
+        # Nginx by default only allows file uploads up to 1M in size
+        # Increase client_max_body_size to match max_upload_size defined in homeserver.yaml
+        client_max_body_size 100M;
+
+	# Synapse responses may be chunked, which is an HTTP/1.1 feature.
+	proxy_http_version 1.1;
+}

submatrix/homeserver.log.config
@@ -1,0 +1,24 @@
+# Log configuration for Synapse.
+
+version: 1
+
+formatters:
+    precise:
+        format: '%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(request)s - %(message)s'
+
+handlers:
+    console:
+        class: logging.StreamHandler
+        formatter: precise
+
+loggers:
+    synapse.storage.SQL:
+        # beware: increasing this to DEBUG will make synapse log sensitive
+        # information such as access tokens.
+        level: INFO
+
+root:
+    level: INFO
+    handlers: [console]
+
+disable_existing_loggers: false

submatrix/homeserver.yaml.template
@@ -1,0 +1,28 @@
+server_name: "${MATRIX_HOSTNAME}"
+pid_file: /tmp/homeserver.pid
+listeners:
+  - port: 8008
+    type: http
+    x_forwarded: true
+    resources:
+      - names:
+        - client
+        - federation
+        compress: false
+database:
+  name: sqlite3
+  args:
+    database: /var/synapse/homeserver.db
+log_config: "/etc/synapse/homeserver.log.config"
+max_upload_size: 100M
+media_store_path: /var/synapse/media_store
+report_stats: true
+signing_key_path: "/var/synapse/${MATRIX_HOSTNAME}.signing.key"
+federation_domain_whitelist:
+  - underground.software # TODO also harden at firewall level
+trusted_key_servers: []
+modules:
+  - module: orbit_auth.OrbitAuthProvider
+    config: {}
+
+# vim:ft=yaml

submatrix/orbit_auth.py
@@ -1,0 +1,79 @@
+import sys
+import pycurl
+from synapse import types
+
+
+def check_credentials(username, password):
+    auth_url = 'http://orbit:9098/mail_auth'
+    headers = [
+        f'Auth-User: {username}',
+        f'Auth-Pass: {password}',
+        'Auth-Protocol: smtp',
+        'Auth-Method: plain',
+    ]
+
+    credential_client = pycurl.Curl()
+    credential_client.setopt(credential_client.URL, auth_url)
+    credential_client.setopt(credential_client.HTTPHEADER, headers)
+
+    try:
+        credential_client.perform()
+        return 200 == credential_client.getinfo(pycurl.RESPONSE_CODE)
+    except pycurl.error as e:
+        print(f'error: request failed: {e}', file=sys.stderr)
+        return False
+    finally:
+        credential_client.close()
+
+
+class OrbitAuthProvider:
+    def __init__(self, _, api):
+
+        self.api = api
+
+        api.register_password_auth_provider_callbacks(
+                auth_checkers={("m.login.password", ("password",)):
+                               self.handle_login})
+
+    async def handle_login(self, input_username, login_type, login_dict):
+        # This should be impossible, because we only provide this
+        # callback for m.login.passwordlogin attempts, but it is worth checking
+        if login_type != "m.login.password":
+            return None
+
+        # Verify that the input string can be parsed as a matrix username and
+        # that the domain name is sane
+        if not types.UserID.is_valid(input_username):
+            return None
+
+        # Should not throw because valid check passed above
+        user_id = types.UserID.from_string(input_username)
+
+        # Check that username contains a hostname, and
+        # that that hostname matches the matrix server
+        if not self.api.is_mine(user_id):
+            return None
+
+        # given a username in the canonical format of "@<username>:<domain>"
+        # only the username part is the Orbit ID. No empty usernames allowed
+        if not (username := user_id.localpart):
+            return None
+
+        # No empty passwords allowed
+        if not (password := login_dict.get("password", None)):
+            return None
+
+        # Ask orbit whether these credentials are valid
+        if not check_credentials(username, password):
+            return None
+
+        # Look up existing record for user (matrix usernames are not
+        # case sensistive) and return canonical capitalization of it
+        existing_user = await self.api.check_user_exists(user_id.to_string())
+        if existing_user is not None:
+            return (existing_user, None)
+
+        # If there is no existing user, lazily register their orbit ID with
+        # synapse so that orbit maintains absolute control over credentials
+        new_user = await self.api.register_user(username, username)
+        return (new_user, None)

test.sh
@@ -28,6 +28,7 @@ DOCKER=${DOCKER:-podman}
 require() { command -v "$1" > /dev/null || { echo "error: $1 command required yet absent" ; exit 1 ; } ; }
 require curl
 require jq
+require flake8
 require "${DOCKER}"
 
 # Check for shell script style compliance with shellcheck
@@ -37,6 +38,8 @@ require "${DOCKER}"
 pushd orbit
 ./test-style.sh
 popd
+
+flake8 submatrix/orbit_auth.py
 
 # Create test dir if it does not exist yet
 mkdir -p test
@@ -223,3 +226,34 @@ curl --url "pop3s://$SINGULARITY_HOSTNAME/1" \
   --user user:pass \
   | tee test/pop_get_message \
   | grep "Bottom text"
+
+# If you get a 429 error from one of the matrix tests, restart the server and try again
+# Synapse has rate-limiting behavior for login requests indicated by this 429 HTTP code
+
+# Check that the user can login to matrix with their orbit ID
+curl --url "https://$SINGULARITY_HOSTNAME/_matrix/client/r0/login" \
+  --unix-socket ./socks/https.sock \
+  "${CURL_OPTS[@]}" \
+  --header "Content-Type: application/json" \
+  --data "{
+        \"type\": \"m.login.password\",
+        \"user\": \"@user:$SINGULARITY_HOSTNAME\",
+        \"password\": \"pass\"
+      }" \
+  | tee test/matrix_login_success \
+  | grep "access_token"
+
+# Check that the user cannot login to matrix with an invalid orbit ID
+curl --url "https://$SINGULARITY_HOSTNAME/_matrix/client/r0/login" \
+  --unix-socket ./socks/https.sock \
+  --verbose \
+  --cacert test/ca_cert.pem \
+  --no-progress-meter \
+  --header "Content-Type: application/json" \
+  --data "{
+        \"type\": \"m.login.password\",
+        \"user\": \"@user:$SINGULARITY_HOSTNAME\",
+        \"password\": \"ssap\"
+      }" \
+  | tee test/matrix_login_invalid \
+  | grep '{"errcode":"M_FORBIDDEN","error":"Invalid username or password"}'

commit 319fc76f91ff87cafc70b6c61ca99abc38997fc8

submatrix: move orbit auth to new folder in `PYTHONPATH`
Installing the orbit auth code in the global site packages folder is a
fragile approach for making it available to synapse. Unbeknownst to us the
version of python was bumped in alpine which meant that the folder for
installing site packages moved from `/usr/lib/python3.11/site-packages/`
to `/usr/lib/python3.12/site-packages/` which broke our container build.

This was not discovered until I fully dropped my caches and rebuilt from
scratch at which point it manifested not even as a build error, but a
"successful" but totally broken build that crashed immediately on startup
due to submatrix not being able to find the auth code which brought down
nginx too because it couldn't find the submatrix upstream.

A better solution is just to pick our own location to put the code and add
that folder to the `PYTHONPATH` environment variable in a startup script.

Fixes: 175e5a5 ("submatrix: initial version")

submatrix/Containerfile
@@ -6,7 +6,9 @@ COPY homeserver.yaml.template /etc/synapse/homeserver.yaml.template
 
 COPY homeserver.log.config /etc/synapse/homeserver.log.config
 
-COPY orbit_auth.py /usr/lib/python3.11/site-packages/orbit_auth.py
+COPY orbit_auth.py /usr/local/share/submatrix/
+
+COPY start.sh /usr/local/share/submatrix/
 
 ARG MATRIX_HOSTNAME=localhost
 
@@ -16,5 +18,5 @@ VOLUME /var/synapse
 
 USER 100:100
 
-CMD ["synapse_homeserver", "-c", "/etc/synapse/homeserver.yaml"]
+ENTRYPOINT ["/usr/local/share/submatrix/start.sh"]
 

submatrix/start.sh
@@ -1,0 +1,3 @@
+#!/bin/sh
+export PYTHONPATH="/usr/local/share/submatrix:${PYTHONPATH}"
+exec synapse_homeserver -c /etc/synapse/homeserver.yaml