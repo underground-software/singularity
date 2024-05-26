5649c8ebd03474cedf7e0016c3b736cc7abb182b
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-05-26 17:08:04 -0400

    chronus: restrict/allow access based on whether students submitted
    This is bootleg, but it basically works.

--- chronus/final.py
+++ chronus/final.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
-print(__import__('sys').argv)
+
+import os
+
+import orbit.db
+
+# Block all students from seeing emails sent until
+# we allow again after initial sub deadline
+for user in orbit.db.User.select():
+    os.system(f'restrict_access /var/lib/email/journal/journal -d {user.username}')
--- chronus/initial.py
+++ chronus/initial.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
-print(__import__('sys').argv)
+
+import os
+import sys
+
+import denis.db
+import orbit.db
+
+
+# this is passed from start.py via run-at
+assignment = sys.argv[1]
+
+for user in orbit.db.User.select():
+    sub = (denis.db.Submission
+           .get_or_none((denis.db.Submission.user == user.username) &
+                        (denis.db.Submission.assignment == assignment)))
+    # let them see emails that have been sent since last final due date
+    if sub is not None:
+        os.system(f'restrict_access /var/lib/email/journal/journal -a {user.username}')
