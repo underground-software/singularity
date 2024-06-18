underground-software/singularity

commit 5222f9764ea03ad6a3c8779c37aa32d0bf798c66

orbit: hyperspace: add the ability clear a password hash
if a user has a null password hash they will not be able to log in
so this can be used to lock the account. It can be unlocked again
by setting a password with `hyperspace --mutate-password`.

orbit/hyperspace.py
@@ -49,6 +49,15 @@ def do_change_password(args):
         nou(args.username)
 
 
+def do_reset_password(args):
+    need(args, u=True)
+    query = (db.User
+             .update({db.User.pwdhash: None})
+             .where(db.User.username == args.username))
+    if query.execute() < 1:
+        nou(args.username)
+
+
 def do_delete_user(args):
     need(args, u=True)
     query = (db.User
@@ -112,6 +121,9 @@ def hyperspace_main(raw_args):
     actions.add_argument('-m', '--mutatepassword', action='store_const',
                          help='Change password for supplied username to supplied password',  # NOQA: E501
                          dest='do', const=do_change_password)
+    actions.add_argument('-c', '--clearpassword', action='store_const',
+                         help='clear password for supplied username so they canot login',  # NOQA: E501
+                         dest='do', const=do_reset_password)
     actions.add_argument('-w', '--withdrawuser', action='store_const',
                          help='Delete ("withdraw") the supplied username',
                          dest='do', const=do_delete_user)

commit b3f96426c7f09f300380a942aecde0496df0b283

orbit: hyperspace: fix typo in help message
canot -> cannot

Fixes: 5222f97 ("orbit: hyperspace: add the ability clear a password hash")

orbit/hyperspace.py
@@ -119,7 +119,7 @@ def hyperspace_main(raw_args):
                          help='Change password for supplied username to supplied password',  # NOQA: E501
                          dest='do', const=do_change_password)
     actions.add_argument('-c', '--clearpassword', action='store_const',
-                         help='clear password for supplied username so they canot login',  # NOQA: E501
+                         help='clear password for supplied username so they cannot login',  # NOQA: E501
                          dest='do', const=do_reset_password)
     actions.add_argument('-w', '--withdrawuser', action='store_const',
                          help='Delete ("withdraw") the supplied username',