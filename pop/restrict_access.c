60ca835675194a4d2dc64cd94f170d0423a3de5c
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-06-18 19:54:26 -0400

    pop: restrict access: allow updating limit
    the new `-u` option provides the ability to atomically update the limit
    that was the old behavior of `-d` before the previous commit.

--- pop/restrict_access.c
+++ pop/restrict_access.c
@@ -7,24 +7,35 @@ static char attr_buf[4096];
 
 int main(int argc, char **argv)
 {
-	if(argc < 4 || argv[2][0]!='-' || (argv[2][1]!='a' && argv[2][1]!='d') || argv[2][2] != '\0')
-		errx(1, "Usage: %s [journal file] [-a|-d] username(s)...", argv[0]);
+	char action;
+	if(argc < 4 || argv[2][0]!='-' || !(action = argv[2][1]) || argv[2][2] != '\0')
+usage:
+		errx(1, "Usage: %s [journal file] [-a|-d|-u] username(s)...", argv[0]);
 	int journalfd = open(argv[1], O_RDWR);
 	if(0 > journalfd)
 		err(1, "Unable to open journal file \"%s\"", argv[1]);
 	off_t limit;
 	if(sizeof limit != fgetxattr(journalfd, "user.data_end", &limit, sizeof limit))
 		err(1, "Unable to read end of data marker from journal file");
-	bool denying = argv[2][1]=='d';
 	for(char **username = &argv[3]; *username; ++username)
 	{
 		if(sizeof attr_buf <= (size_t)snprintf(attr_buf, sizeof attr_buf, "user.%s_limit", *username))
 			errx(1, "Username \"%s\" is too long", *username);
 		int ret;
-		if(denying)
+		switch(action)
+		{
+		case 'u':
+			ret = fsetxattr(journalfd, attr_buf, &limit, sizeof limit, 0);
+			break;
+		case 'd':
 			ret = fsetxattr(journalfd, attr_buf, &limit, sizeof limit, XATTR_CREATE);
-		else
+			break;
+		case 'a':
 			ret = fremovexattr(journalfd, attr_buf);
+			break;
+		default:
+			goto usage;
+		}
 		if(0 > ret)
 			err(1, "Unable to modify attribute \"%s\"", attr_buf);
 	}
