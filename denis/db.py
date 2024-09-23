underground-software/singularity

commit 86353ae5eb071adbe04aaf9066b6e8ad6834d7a7

denis: record peer reveiew assignments
Signed-off-by: Joel Savitz <jsavitz@redhat.com>

denis/initial.py
@@ -7,6 +7,7 @@ import random
 import sys
 
 import config
+import db
 import mailman.db
 import orbit.db
 
@@ -134,6 +135,18 @@ random.shuffle(students_who_submitted)
 reviews = [[students_who_submitted[i+j]
             for j in range(-min(len(students_who_submitted), 3), 0)]
            for i in range(len(students_who_submitted))]
+
+
+try:
+    with db.DB.atomic():
+        db.PeerReviewAssignment.insert_many(
+            [{'assignment': assignment,
+              'reviewer': reviewer,
+              'reviewee1': reviewee1,
+              'reviewee2': reviewee2}
+             for (reviewer, reviewee1, reviewee2) in reviews]).execute()
+except db.peewee.IntegrityError as e:
+    print(e)
 
 # To make it easier for the student to find their row, we can sort the
 # list. This will alphabetize based on the first column (and only the

commit d6f2e253a572a0417ae3012a0bf5b87393e999d6

denis: initial/db: handle < 3 students completing assignment
The schema needs to be able to handle one or both of the reviwees being
null, and the code that inserts into the DB needs to account for lists
that are not exactly 3 elements long.

Fixes: 86353ae ("denis: record peer reveiew assignments")

denis/db.py
@@ -20,8 +20,8 @@ class Assignment(BaseModel):
 class PeerReviewAssignment(BaseModel):
     assignment = peewee.TextField()
     reviewer = peewee.TextField()
-    reviewee1 = peewee.TextField()
-    reviewee2 = peewee.TextField()
+    reviewee1 = peewee.TextField(null=True)
+    reviewee2 = peewee.TextField(null=True)
 
     class Meta:
         indexes = ((('assignment', 'reviewer'), True),)

denis/initial.py
@@ -142,9 +142,9 @@ try:
         db.PeerReviewAssignment.insert_many(
             [{'assignment': assignment,
               'reviewer': reviewer,
-              'reviewee1': reviewee1,
-              'reviewee2': reviewee2}
-             for (reviewer, reviewee1, reviewee2) in reviews]).execute()
+              'reviewee1': reviewees[0] if len(reviewees) >= 1 else None,
+              'reviewee2': reviewees[1] if len(reviewees) >= 2 else None}
+             for [reviewer, *reviewees] in reviews]).execute()
 except db.peewee.IntegrityError as e:
     print(e)
 