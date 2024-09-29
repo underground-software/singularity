aa82726180471b25b307f3b658d55721fd1362d4
Joel Savitz <jsavitz@redhat.com>
2024-09-29 14:00:26 -0400

    mailman: add inspector.sh to examine submissions
    Includes ability to restrict output by assignment or username, as well
    as a 'missing' command to list usernames for which no submission exists
    for a given assignment.
    
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>
    (cherry picked from commit 6891deadae0f041eb1132d1ab3aede844756c61a)
    
    Conflicts:
    	- mailmain.db.Submission table field 'assignment' renamed to
    	  'recipient' in upstream, use Submission.assignment here due to
    	  missing commit e664b44 ("denis: initial: update query to reflect
    	  new schema")

---
+++ mailman/inspector.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from argparse import ArgumentParser as ap
+
+import db
+import orbit.db
+
+
+def main():
+    parser = ap(prog='inspector', description='Inspect submissions')
+
+    command_parsers = parser.add_subparsers(dest='command', required=True,
+                                            description='Action to take')
+
+    submissions_parser = command_parsers.add_parser('submissions')
+    submissions_parser.add_argument('-a', '--assignment',
+                                    help='Restrict results by assignment',
+                                    required=False)
+    submissions_parser.add_argument('-u', '--username',
+                                    help='Restrict results by username',
+                                    required=False)
+
+    missing_parser = command_parsers.add_parser('missing')
+    missing_parser.add_argument('-a', '--assignment',
+                                help='Select the assignment',
+                                required=True)
+
+    # Dictionary containing the desired command and all flags with their values
+    kwargs = vars(parser.parse_args())
+    # Subparsers store their name in the destination `'command'`
+    subparser_name = kwargs.pop('command')
+    # Get a handle to the function with the desired name
+    subparser_func = globals()[subparser_name]
+    # Call the desired subparser with the remaining flags/values as kwargs
+    subparser_func(**kwargs)
+
+
+def submissions(assignment, username):
+    query = db.Submission.select().order_by(-db.Submission.timestamp)
+    if assignment:
+        query = query.where(db.Submission.assignment == assignment)
+    if username:
+        query = query.where(db.Submission.user == username)
+    for sub in query:
+        print(sub.submission_id,
+              datetime.fromtimestamp(sub.timestamp).isoformat(),
+              sub.user, sub.assignment)
+
+
+def missing(assignment):
+    query = db.Submission.select().where(db.Submission.assignment == assignment)  # NOQA: E501
+    submitted = {sub.user for sub in query}
+    for user in orbit.db.User.select():
+        if user.username not in submitted:
+            print(user.username)
+
+
+if __name__ == '__main__':
+    main()
