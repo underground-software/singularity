2a4508068f73569b7cbdb1ec6d11e342b5990721
bashar-osama <bashar-osama@spring2024-iit.kdlp.underground.software>
2024-10-12 17:27:23 +0300

    Add infrastructure for SMTP test coverage verification
    - Integrated gcov and lcov into the project build process to enable test coverage reporting.
    - Updated containerfile/compose to conditionally include gcov during the build, allowing seamless
    generation of coverage data.
    - Verified that test infrastructure correctly captures coverage.
    - This setup facilitates improved test coverage analysis and supports future test development.
    
    Signed-off-by: basharabdelgafer <basharabdelgafer@gmail.com>

--- collect_coverage.sh
+++ collect_coverage.sh
@@ -8,3 +8,19 @@ coverage combine --keep coverage* >/dev/null 2>/dev/null
 coverage html >/dev/null 2>/dev/null
 tar -c htmlcov
 ' | tar -C coverage -x
+
+
+
+CONTAINER_NAME="singularity_smtp_1"
+
+# Ensure the local coverage directory exists
+mkdir -p $LOCAL_COVERAGE_DIR
+
+# Execute commands inside the container to process gcov files
+podman exec -i $CONTAINER_NAME sh -exc '
+    cd /smtp
+    rm -rf default.profdata default.profraw htmlcov_smtp
+    llvm-profdata merge -sparse default.profraw -o default.profdata
+    llvm-cov show ./smtp -instr-profile=default.profdata -format=html -output-dir=htmlcov_coverage_smtp >/dev/null 2>&1
+    tar -c htmlcov_coverage_smtp
+' | tar -C coverage_smtp -x
