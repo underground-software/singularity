underground-software/singularity

commit 4dfa0e40845513a2f84e31f65f79ec8bc87bd46b

orbit: conainerfile: add more packages
fixes #243

orbit/radius.py
@@ -749,6 +749,7 @@ tar \
 make \
 gcc \
 qemu-system-riscv \
+qemu-user-static-ricsv \
 binutils-riscv64-linux-gnu \
 gcc-riscv64-linux-gnu \
 bc \
@@ -769,6 +770,8 @@ cyrus-sasl-plain \
 gdb \
 diffutils \
 strace \
+man \
+man-pages \
 ;
 dnf clean all
 DNF

commit 3a7fdd0880fff2a9d97f229ec944d0376032a298

nit: typo in package name
friendship with ricsv ended, now riscv is my best friend.

Fixes: 4dfa0e4 ("orbit: conainerfile: add more packages")

orbit/radius.py
@@ -748,7 +748,7 @@ git \
 tar \
 make \
 gcc \
-qemu-system-riscv \
+qemu-system-ricsv \
 qemu-user-static-ricsv \
 binutils-riscv64-linux-gnu \
 gcc-riscv64-linux-gnu \