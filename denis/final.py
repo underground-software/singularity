7aa57724cccb924600833db2daf0af22de4b370f
Joel Savitz <joel@underground.software>
2025-05-22 18:35:50 -0400

    denis,orbit: give corrupt or missing patches an automatic zero
    On the relevant deadline trigger, assign a zero score to a submission
    when mailman receives none or corrupt patches.
    
    Fixes #207
    
    Signed-off-by: Joel Savitz <joel@underground.software>

--- denis/final.py
+++ denis/final.py
@@ -5,12 +5,12 @@ import utilities
 
 assignment = sys.argv[1]
 
-utilities.release_subs([sub for sub in
-                        utilities.user_to_sub(assignment, 'final').values()
-                        if sub])
+usernames_to_subs = utilities.user_to_sub(assignment, 'final')
+
+utilities.release_subs([sub.submission_id for sub in usernames_to_subs.values() if sub])
 
 print(f'final subs for {assignment} released')
 
 tags = utilities.update_tags(assignment, 'final')
 
-utilities.run_automated_checks(tags)
+utilities.run_automated_checks(tags, usernames_to_subs)
