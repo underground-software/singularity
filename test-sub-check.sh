76b69b2cf02f3638c86e407de245c9bf07909731
Joel Savitz <joel@underground.software>
2025-05-30 20:08:36 -0400

    test-sub-check.sh: add script to check results of test-sub.sh
    test-sub.sh is good for interactive testing, but lacks validation when
    automated. Add test-sub-check.sh to run some basic checks that make sure
    test-sub.sh ran smoothly.
    
    Signed-off-by: Joel Savitz <joel@underground.software>

---
+++ test-sub-check.sh
@@ -0,0 +1,118 @@
+#!/usr/bin/env bash
+
+CURL_OPTS=( \
+--verbose \
+--cacert test/ca_cert.pem \
+--fail \
+--no-progress-meter \
+)
+
+set -exuo pipefail
+
+HOSTNAME_FROM_DOTENV="$(sh -c '
+set -o allexport
+. ./.env
+exec jq -r -n "env.SINGULARITY_HOSTNAME"
+')"
+
+DOCKER=${DOCKER:-podman}
+
+SINGULARITY_HOSTNAME=${SINGULARITY_HOSTNAME:-"${HOSTNAME_FROM_DOTENV}"}
+
+# Create test dir if it does not exist yet
+mkdir -p test
+
+# Reset the test directory
+rm -f test/*
+
+${DOCKER} cp singularity_nginx_1:/etc/ssl/nginx/fullchain.pem test/ca_cert.pem
+
+# login as bob
+curl --url "https://$SINGULARITY_HOSTNAME/login" \
+  --unix-socket ./socks/https.sock \
+  "${CURL_OPTS[@]}" \
+  -c test/cookies \
+  --data "username=bob&password=builder" \
+  | tee test/login_success \
+  | grep "msg = bob authenticated by password"
+
+
+# check for setup assignment and save dashboard
+curl --url "https://$SINGULARITY_HOSTNAME/dashboard" \
+  --unix-socket ./socks/https.sock \
+  -b test/cookies \
+  "${CURL_OPTS[@]}" \
+  | tee test/dashboard \
+  | grep "setup"
+
+grep "Total Score: 64.9" test/dashboard
+
+grep "missing cover letter and first patch failed to apply!" test/dashboard
+
+grep "bib Peer Review" test/dashboard
+
+grep "bab Peer Review" test/dashboard
+
+grep "patchset applies." test/dashboard
+
+grep "Good effort but try harder next time." test/dashboard
+
+# login as bab
+curl --url "https://$SINGULARITY_HOSTNAME/login" \
+  --unix-socket ./socks/https.sock \
+  "${CURL_OPTS[@]}" \
+  -c test/cookies_bab \
+  --data "username=bab&password=builder" \
+  | tee test/login_success_bab \
+  | grep "msg = bab authenticated by password"
+
+
+# check for setup assignment and save dashboard
+curl --url "https://$SINGULARITY_HOSTNAME/dashboard" \
+  --unix-socket ./socks/https.sock \
+  -b test/cookies_bab \
+  "${CURL_OPTS[@]}" \
+  | tee test/dashboard_bab \
+  | grep "setup"
+
+grep "Total Score: 0.0" test/dashboard_bab
+
+grep "bib Peer Review" test/dashboard_bab
+
+grep "bob Peer Review" test/dashboard_bab
+
+grep "patchset applies." test/dashboard_bab
+
+grep "illegal patch 1: permission denied for path work!" test/dashboard_bab
+
+grep 'Please review git' test/dashboard_bab
+
+# login as bib
+curl --url "https://$SINGULARITY_HOSTNAME/login" \
+  --unix-socket ./socks/https.sock \
+  "${CURL_OPTS[@]}" \
+  -c test/cookies_bib \
+  --data "username=bib&password=builder" \
+  | tee test/login_success_bib \
+  | grep "msg = bib authenticated by password"
+
+
+# check for setup assignment and save dashboard
+curl --url "https://$SINGULARITY_HOSTNAME/dashboard" \
+  --unix-socket ./socks/https.sock \
+  -b test/cookies_bib \
+  "${CURL_OPTS[@]}" \
+  | tee test/dashboard_bib \
+  | grep "setup"
+
+grep "Total Score: 80.0" test/dashboard_bib
+
+grep "bob Peer Review" test/dashboard_bib
+
+grep "bab Peer Review" test/dashboard_bib
+
+grep "patchset applies." test/dashboard_bib
+
+grep 'Perfect. But no peer review!' test/dashboard_bib
+
+echo "ALL SUBMISSION TESTS PASS"
