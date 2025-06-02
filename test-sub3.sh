e505d013582b0bbc7a5e3d0e4814467829cb2000
Joel Savitz <joel@underground.software>
2025-06-02 15:02:54 -0400

    test: add test for whether only initial submission gets autozero
    If a student makes an inital submission but not a final submission, they
    should automatically receive a zero for the assignment.
    
    Signed-off-by: Joel Savitz <joel@underground.software>

---
+++ test-sub3.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# test only initial submission results in automatic 0
+
+source test-lib
+
+setup_submissions_and_grading_repo
+
+create_lighting_assignment fun 5 7 10
+
+setup_submissions_for bob
+
+enter_and_checkout fun_initial_bob_only-initial
+write_commit_to "C program" bob/fun/program.c
+write_commit_to "More C program" bob/fun/program.c "append"
+write_commit_to "Yet more C program" bob/fun/program.c "append"
+git format-patch -v1 --cover-letter --rfc -3
+fixup_cover "Only initial sub patchset"
+exit_after_sending fun
+
+# wait for initial submission deadline
+sleep 9
+
+setup_testdir
+
+# login as bob
+curl --url "https://$SINGULARITY_HOSTNAME/login" \
+  --unix-socket ./socks/https.sock \
+  "${CURL_OPTS[@]}" \
+  -c test/cookies \
+  --data "username=bob&password=builder" \
+  | tee test/login_success \
+  | grep "msg = bob authenticated by password"
+
+
+# check for fun assignment and save dashboard
+curl --url "https://$SINGULARITY_HOSTNAME/dashboard" \
+  --unix-socket ./socks/https.sock \
+  -b test/cookies \
+  "${CURL_OPTS[@]}" \
+  | tee test/dashboard \
+  | grep "fun"
+
+grep "patchset applies." test/dashboard
+grep "No submission" test/dashboard
+grep "Total Score: 0.0" test/dashboard
+
+echo "INITIAL SUBMISSION ONLY GETS AUTO ZERO CONFIRMED"
