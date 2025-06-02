d2375f41fc000ae9493c02c2ff417b25df0ba642
Joel Savitz <joel@underground.software>
2025-06-01 22:21:32 -0400

    test: add more rubric tests cleanly by consolidating common testing code
    Add test-sub2.sh which tests out 12 invalid and 1 valid submission on a
    rubric for an example coding assignment and validate the resulting
    status values added to the email ID tags in the grading repo.
    
    Simplify the new tests by abstracting the setup section and repetitive
    commands, enabling quicker development of shorter future testing
    scripts the same flavor as the rest of test-sub*.sh. This is important
    as we want to make sure our automated grading features actually do what
    we think they will do to the fullest extent possible before this code
    goes into production with real students.
    
    Signed-off-by: Joel Savitz <joel@underground.software>

--- test-sub.sh
+++ test-sub.sh
@@ -1,283 +1,90 @@
 #!/bin/bash
 
-set -exuo pipefail
+# assumes singularity is running when invoked
+# script is written to be as idempotent as posible
 
-SCRIPT_DIR=$(dirname "$0")
+source test-lib
 
-cd "$SCRIPT_DIR"
-
-get_git_port() { podman port singularity_git_1 | awk -F':' '{ print $2 }' ; } ;
-
-# nuke grading repo inside container
-# shellcheck disable=SC2016
-podman-compose exec git ash -c 'cd /var/lib/git/grading.git && for t in $(git tag); do git tag -d $t; done'
-
-# setup submissions repo
-rm -rf repos/submissions
-git/admin.sh submissions "course submissions repository"
-pushd repos
-git init --bare submissions
-echo "course submissions repository" > submissions/description
-git init submissions_init
-pushd submissions_init
-echo "# submissions" > README.md
-git add README.md
-git -c user.name=singularity -c user.email=singularity@singularity commit -m 'init submissions repo'
-git push ../submissions master
-popd
-rm -rf submissions_init
-pushd submissions
-git push --mirror http://localhost:"$(get_git_port)"/cgi-bin/git-receive-pack/submissions
-popd
-popd
-
-# create bob,bib,bab users or change their passwords to builder
-orbit/warpdrive.sh -u bob -p builder -n || orbit/warpdrive.sh -u bob -p builder -m
-orbit/warpdrive.sh -u bib -p builder -n || orbit/warpdrive.sh -u bob -p builder -m
-orbit/warpdrive.sh -u bab -p builder -n || orbit/warpdrive.sh -u bob -p builder -m
-
-WORKDIR=$(mktemp -d)
+setup_submissions_and_grading_repo
 
 cat << EOF > "$WORKDIR"/setup_rubric
 [{('--- /dev/null', '+++ b/bob/setup/work'): 0},
 {('--- a/bob/setup/work', '+++ b/bob/setup/work'): 0}]
 EOF
 
-# create or recreate setup assignment
-if denis/configure.sh dump | grep -q "^setup:"; then
-	denis/configure.sh remove -a setup
-fi
-denis/configure.sh create -a setup -i "$(date -d "15 secs" +%s)" -p "$(date -d "25 secs" +%s)" -f "$(date -d "30 secs" +%s)" -r "$WORKDIR"/setup_rubric
-denis/configure.sh reload
-
-# setup assignment environment
-pushd "$WORKDIR"
-
-mkdir certs
-pushd certs
-podman volume export singularity_ssl-certs > certs.tar
-tar xf certs.tar
-popd
-git clone http://localhost:"$(get_git_port)"/submissions
-
-# bob env setup
-
-pushd submissions
-git config user.name bob
-git config user.email bob@localhost.localdomain
-git config sendemail.smtpUser bob
-git config sendemail.smtpPass builder
-git config sendemail.smtpserver localhost.localdomain
-git config sendemail.smtpserverport 465
-git config sendemail.smtpencryption ssl
-popd
+create_lighting_assignment setup 15 25 30 "$WORKDIR"/setup_rubric
 
 # setup_intial_bob patchsets
+setup_submissions_for bob
 
 # good patchset
-pushd submissions
-git checkout --orphan setup_initial_bob_good
-git rm -rf ./*
-mkdir -p bob/setup
-
-echo "abc" > bob/setup/work
-git add bob/setup/work
-git commit -sm 'add abc to work'
-
-echo "def" >> bob/setup/work
-git add bob/setup/work
-git commit -sm 'add def to work'
-
+enter_and_checkout setup_initial_bob_good
+write_commit_to "abc" bob/setup/work
+write_commit_to "def" bob/setup/work "append"
 git format-patch --rfc --cover-letter -v1 -2
-sed -i 's/\*\*\* SUBJECT HERE \*\*\*/Good patchset/g' v1-0000-cover-letter.patch
-sed -i 's/\*\*\* BLURB HERE \*\*\*/Good patchset/g' v1-0000-cover-letter.patch
-
-git send-email --confirm=never --smtp-ssl-cert-path="$WORKDIR"/certs/fullchain.pem --to setup@localhost.localdomain ./*.patch
-rm ./*.patch
-popd
-sleep 1
+fixup_cover "Good patchset"
+exit_after_sending setup
 
 # corrupt patchset
-pushd submissions
-git checkout --orphan setup_initial_bob_corrupt
-git rm -rf ./*
-mkdir -p bob/setup
-
-echo "abc" > bob/setup/work
-git add bob/setup/work
-git commit -sm 'add abc to work'
-
-echo "def" >> bob/setup/work
-git add bob/setup/work
-git commit -sm 'add def to work'
-
-echo "ghi" >> bob/setup/work
-git add bob/setup/work
-git commit -sm 'add ghi to work'
-
+enter_and_checkout setup_initial_bob_corrupt
+write_commit_to "abc" bob/setup/work
+write_commit_to "def" bob/setup/work "append"
+write_commit_to "ghi" bob/setup/work "append"
 git format-patch --rfc --cover-letter -v1 -3
 rm v1-0002-*.patch
-
-sed -i 's/\*\*\* SUBJECT HERE \*\*\*/Corrupt patchset/g' v1-0000-cover-letter.patch
-sed -i 's/\*\*\* BLURB HERE \*\*\*/Corrupt patchset/g' v1-0000-cover-letter.patch
-
-git send-email --confirm=never --smtp-ssl-cert-path="$WORKDIR"/certs/fullchain.pem --to setup@localhost.localdomain ./*.patch
-rm ./*.patch
-popd
-sleep 1
+fixup_cover "Corrupt patchset"
+exit_after_sending setup
 
 # patchset with whitespace errors
-pushd submissions
-git checkout --orphan setup_initial_bob_whitespace
-git rm -rf ./*
-mkdir -p bob/setup
-
-echo "abc" > bob/setup/work
-git add bob/setup/work
-git commit -sm 'add abc to work'
-
-echo "def	" >> bob/setup/work
-git add bob/setup/work
-git commit -sm 'add def to work'
-
+enter_and_checkout setup_initial_bob_whitespace
+write_commit_to "abc" bob/setup/work
+write_commit_to "def	" bob/setup/work "append"
 git format-patch --rfc --cover-letter -v1 -2
-sed -i 's/\*\*\* SUBJECT HERE \*\*\*/Patchset with whitespace errors/g' v1-0000-cover-letter.patch
-sed -i 's/\*\*\* BLURB HERE \*\*\*/Patchset with whitespace errors/g' v1-0000-cover-letter.patch
-
-git send-email --confirm=never --smtp-ssl-cert-path="$WORKDIR"/certs/fullchain.pem --to setup@localhost.localdomain ./*.patch
-rm ./*.patch
-popd
-sleep 1
+fixup_cover "Patchset with whitespace errors"
+exit_after_sending setup
 
 # patchset with no cover letter
-pushd submissions
-git checkout --orphan setup_initial_bob_nocover
-git rm -rf ./*
-mkdir -p bob/setup
-
-echo "abc" > bob/setup/work
-git add bob/setup/work
-git commit -sm 'add abc to work'
-
-echo "def" >> bob/setup/work
-git add bob/setup/work
-git commit -sm 'add def to work'
-
+enter_and_checkout setup_initial_bob_nocover
+write_commit_to "abc" bob/setup/work
+write_commit_to "def" bob/setup/work "append"
 git format-patch --rfc -v1 -2
-
-git send-email --confirm=never --smtp-ssl-cert-path="$WORKDIR"/certs/fullchain.pem --to setup@localhost.localdomain ./*.patch
-rm ./*.patch
-popd
-sleep 1
+exit_after_sending setup
 
 # patchset with no cover letter and corrupt first patch
-pushd submissions
-git checkout --orphan setup_initial_bob_nocover-corrupt
-git rm -rf ./*
-mkdir -p bob/setup
-
-echo "abc" > bob/setup/work
-git add bob/setup/work
-git commit -sm 'add abc to work'
-
-echo "def" >> bob/setup/work
-git add bob/setup/work
-git commit -sm 'add def to work'
-
-echo "ghi" >> bob/setup/work
-git add bob/setup/work
-git commit -sm 'add ghi to work'
-
+enter_and_checkout setup_initial_bob_nocover-corrupt
+write_commit_to "abc" bob/setup/work
+write_commit_to "def" bob/setup/work "append"
+write_commit_to "ghi" bob/setup/work "append"
 git format-patch --rfc -v1 -2
-
-git send-email --confirm=never --smtp-ssl-cert-path="$WORKDIR"/certs/fullchain.pem --to setup@localhost.localdomain ./*.patch
-rm ./*.patch
-popd
-
-# end of setup_initial_bob patchsets
-
-# bab env setup
-
-pushd submissions
-git config user.name bab
-git config user.email bab@localhost.localdomain
-git config sendemail.smtpUser bab
-git config sendemail.smtpPass builder
-git config sendemail.smtpserver localhost.localdomain
-git config sendemail.smtpserverport 465
-git config sendemail.smtpencryption ssl
-popd
+exit_after_sending setup
 
 # setup_initial_bab submissions
+setup_submissions_for bab
 
 # bab good patchset
-pushd submissions
-git checkout --orphan setup_initial_bab_good
-git rm -rf ./*
-mkdir -p bab/setup
-
-echo "abc" > bab/setup/work
-git add bab/setup/work
-git commit -sm 'add abc to work'
-
-echo "def" >> bab/setup/work
-git add bab/setup/work
-git commit -sm 'add def to work'
-
+enter_and_checkout setup_initial_bab_good
+write_commit_to "abc" bab/setup/work
+write_commit_to "def" bab/setup/work "append"
 git format-patch --rfc --cover-letter -v1 -2
-sed -i 's/\*\*\* SUBJECT HERE \*\*\*/bab: Good patchset/g' v1-0000-cover-letter.patch
-sed -i 's/\*\*\* BLURB HERE \*\*\*/bab: Good patchset/g' v1-0000-cover-letter.patch
+fixup_cover "Good patchset"
+exit_after_sending setup
 
-git send-email --confirm=never --smtp-ssl-cert-path="$WORKDIR"/certs/fullchain.pem --to setup@localhost.localdomain ./*.patch
-rm -rf ./*.patch
-popd
-
-# end of bab initial submission
-
-# bib env setup
-pushd submissions
-git config user.name bib
-git config user.email bib@localhost.localdomain
-git config sendemail.smtpUser bib
-git config sendemail.smtpPass builder
-git config sendemail.smtpserver localhost.localdomain
-git config sendemail.smtpserverport 465
-git config sendemail.smtpencryption ssl
-popd
-
-# bib initial submissions
+# setup_initial_bib submissions
+setup_submissions_for bib
 
 # bib good patchset
-pushd submissions
-git checkout --orphan setup_initial_bib_good
-git rm -rf ./*
-mkdir -p bib/setup
-
-echo "abc" > bib/setup/work
-git add bib/setup/work
-git commit -sm 'add abc to work'
-
-echo "def" >> bib/setup/work
-git add bib/setup/work
-git commit -sm 'add def to work'
-
+enter_and_checkout setup_initial_bib_good
+write_commit_to "abc" bib/setup/work
+write_commit_to "def" bib/setup/work "append"
 git format-patch --rfc --cover-letter -v1 -2
-sed -i 's/\*\*\* SUBJECT HERE \*\*\*/bib: Good patchset/g' v1-0000-cover-letter.patch
-sed -i 's/\*\*\* BLURB HERE \*\*\*/bib: Good patchset/g' v1-0000-cover-letter.patch
-
-git send-email --confirm=never --smtp-ssl-cert-path="$WORKDIR"/certs/fullchain.pem --to setup@localhost.localdomain ./*.patch
-rm ./*.patch
-popd
-sleep 1
-
-# end of bib initial submission
+fixup_cover "Good patchset"
+exit_after_sending setup
 
 # investigate grading repo
+pushd "$WORKDIR"
 git clone http://localhost:"$(get_git_port)"/grading.git
 pushd grading
 git fetch --tag
-git tag
-
 
 STATUSES=(
 'patchset applies.'
@@ -300,36 +107,17 @@ for t in $(git tag); do
 	fi
 	i=$((i+1))
 done
-
 popd
 
-echo "wait for initial submission deadline (10 secs)"
-sleep 10
+echo "wait for initial submission deadline (5 secs)"
+sleep 5
 
-# bob env setup
-
-pushd submissions
-git config user.name bob
-git config user.email bob@localhost.localdomain
-git config sendemail.smtpUser bob
-git config sendemail.smtpPass builder
-git config sendemail.smtpserver localhost.localdomain
-git config sendemail.smtpserverport 465
-git config sendemail.smtpencryption ssl
-popd
-
+setup_submissions_for bob
 
 # setup bob peer review
-pushd submissions
-git checkout --orphan setup_reviews_bob
-git rm -rf ./*
-# mkdir -p bob/setup (must be able to omit if missing)
-echo ORPHAN > ORPHAN
-git add ORPHAN
-git commit -m 'orphan base'
+enter_and_checkout setup_reviews_bob
 
 # submit peer review 1
-
 cat <<PEER_REVIEW1 > review1
 Subject: setup review 1 for bab
 In-Reply-To: <${PEER1_ID::-1}1@localhost.localdomain>
@@ -338,14 +126,10 @@ Looks good to me
 
 Acked-by: bob <bob@localhost.localdomain>
 PEER_REVIEW1
-
 git send-email --confirm=never --smtp-ssl-cert-path="$WORKDIR"/certs/fullchain.pem --to bab@localhost.localdomain review1
 rm review1
 
-# end peer review 1
-
 # submit peer review 2
-
 cat <<PEER_REVIEW2 > review2
 Subject: setup review 2 for bib
 In-Reply-To: <${PEER2_ID::-1}2@localhost.localdomain>
@@ -354,131 +138,39 @@ Looks good to me
 
 Acked-by: bob <bob@localhost.localdomain>
 PEER_REVIEW2
-
 git send-email --confirm=never --smtp-ssl-cert-path="$WORKDIR"/certs/fullchain.pem --to bib@localhost.localdomain review2
 rm review2
 
-# end peer review 2
-
+popd
+sleep 1
 # end bob peer review
-popd
-sleep 1
 
-# bob env setup
-
-pushd submissions
-git config user.name bob
-git config user.email bob@localhost.localdomain
-git config sendemail.smtpUser bob
-git config sendemail.smtpPass builder
-git config sendemail.smtpserver localhost.localdomain
-git config sendemail.smtpserverport 465
-git config sendemail.smtpencryption ssl
-popd
-
-# bob make final submission
-
-# good final patchset
-pushd submissions
-git checkout --orphan setup_final_bob_good
-git rm -rf ./*
-mkdir -p bob/setup
-
-echo "abc" > bob/setup/work
-git add bob/setup/work
-git commit -sm 'add abc to work'
-
-echo "def" >> bob/setup/work
-git add bob/setup/work
-git commit -sm 'add def to work'
-
+# setup_final_bob good patchset
+setup_submissions_for bob
+enter_and_checkout setup_final_bob_good
+write_commit_to "abc" bob/setup/work
+write_commit_to "def" bob/setup/work "append"
 git format-patch --cover-letter -v2 -2
-sed -i 's/\*\*\* SUBJECT HERE \*\*\*/Good patchset final/g' v2-0000-cover-letter.patch
-sed -i 's/\*\*\* BLURB HERE \*\*\*/Good patchset final/g' v2-0000-cover-letter.patch
+fixup_cover "Good final patchset"
+exit_after_sending setup
 
-git send-email --confirm=never --smtp-ssl-cert-path="$WORKDIR"/certs/fullchain.pem --to setup@localhost.localdomain ./*.patch
-rm ./*.patch
-popd
-sleep 1
-
-# end bob final submission
-
-# bib env setup
-
-pushd submissions
-git config user.name bib
-git config user.email bib@localhost.localdomain
-git config sendemail.smtpUser bib
-git config sendemail.smtpPass builder
-git config sendemail.smtpserver localhost.localdomain
-git config sendemail.smtpserverport 465
-git config sendemail.smtpencryption ssl
-popd
-
-# bib final submission labeled RFC
-pushd submissions
-git checkout --orphan setup_final_bib_rfc
-git rm -rf ./*
-mkdir -p bib/setup
-
-echo "abc" > bib/setup/work
-git add bib/setup/work
-git commit -sm 'add abc to work'
-
-echo "def" >> bib/setup/work
-git add bib/setup/work
-git commit -sm 'add def to work'
-
+# setup_final_bib final submission incorrectly labeled RFC
+setup_submissions_for bib
+enter_and_checkout setup_final_bib_rfc
+write_commit_to "abc" bib/setup/work
+write_commit_to "def" bib/setup/work "append"
 git format-patch --rfc --cover-letter -v1 -2
-sed -i 's/\*\*\* SUBJECT HERE \*\*\*/bib: RFC patchset/g' v1-0000-cover-letter.patch
-sed -i 's/\*\*\* BLURB HERE \*\*\*/bib: RFC patchset/g' v1-0000-cover-letter.patch
+fixup_cover "RFC tagged final patchset"
+exit_after_sending setup
 
-git send-email --confirm=never --smtp-ssl-cert-path="$WORKDIR"/certs/fullchain.pem --to setup@localhost.localdomain ./*.patch
-rm ./*.patch
-popd
-sleep 1
-
-# end of bib final submission labeled RFC
-
-
-# bab env setup
-
-pushd submissions
-git config user.name bab
-git config user.email bab@localhost.localdomain
-git config sendemail.smtpUser bab
-git config sendemail.smtpPass builder
-git config sendemail.smtpserver localhost.localdomain
-git config sendemail.smtpserverport 465
-git config sendemail.smtpencryption ssl
-popd
-
-
-# bab final submission in wrong directory
-pushd submissions
-git checkout --orphan setup_final_bab_illegal
-git rm -rf ./*
-mkdir -p bib/setup
-
-echo "abc" > work
-git add work
-git commit -sm 'add abc to work'
-
-echo "def" >> work
-git add work
-git commit -sm 'add def to work'
-
+# setup_final_bab submission in wrong directory
+setup_submissions_for bab
+enter_and_checkout setup_final_bab_illegal
+write_commit_to "abc" work
+write_commit_to "def" work "append"
 git format-patch --rfc --cover-letter -v1 -2
-sed -i 's/\*\*\* SUBJECT HERE \*\*\*/bab: Illegal patchset/g' v1-0000-cover-letter.patch
-sed -i 's/\*\*\* BLURB HERE \*\*\*/bab: Illegal patchset/g' v1-0000-cover-letter.patch
-
-git send-email --confirm=never --smtp-ssl-cert-path="$WORKDIR"/certs/fullchain.pem --to setup@localhost.localdomain ./*.patch
-rm ./*.patch
-popd
-sleep 1
-
-# end of bab final submission in wrong directory
-
+fixup_cover "Illegal patchset"
+exit_after_sending setup
 
 echo "wait for final submission deadline (10 secs)"
 sleep 10
@@ -488,14 +180,17 @@ git fetch --tag
 sleep 2
 git fetch -f origin refs/notes/*:refs/notes/*
 git remote set-url --push origin http://localhost:"$(get_git_port)"/cgi-bin/git-receive-pack/grading.git
+
 git notes --ref=grade add setup_final_bob -m '66'
 git notes --ref=grade add setup_review1_bob -m '22'
 git notes --ref=grade add setup_review2_bob -m '99'
 git notes --ref=feedback add setup_final_bob -m 'Good effort but try harder next time.'
 
+# automatic 0 for peer reviews
 git notes --ref=grade add setup_final_bib -m '100'
 git notes --ref=feedback add setup_final_bib -m 'Perfect. But no peer review!'
 
+# automatic 0 for peer reviews and final submission
 git notes --ref=feedback add setup_final_bab -m 'Please review git'
 
 git push origin refs/notes/*:refs/notes/*
