2bae10d24ac51dc959155b1d21a3e2e1da0e3865
Joel Savitz <joel@underground.software>
2025-08-07 00:47:34 -0400

    api: add version endpoint
    Signed-off-by: Joel Savitz <joel@underground.software>

--- api/api.py
+++ api/api.py
@@ -1,4 +1,15 @@
+from config import version_info
+import json
+from urllib.parse import parse_qs
+
+
 def application(env, SR):
-    headers = [('Content-Type', 'text/plain')]
-    SR(f'200 OK', headers)
-    return [b'API']
+    headers = [('Content-Type', 'application/json')]
+    SR('200 OK', headers)
+    queries = parse_qs(env.get("QUERY_STRING", ""))
+    path_info = env.get("PATH_INFO", "/")
+    match path_info.split('/'):
+        case ['', 'api', 'version']:
+            return [json.dumps({'version': version_info}).encode()]
+        case _:
+            return [json.dumps({'error': 'Invalid endpoint'}).encode()]
