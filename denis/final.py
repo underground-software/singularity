88bca31bafc8a67263993b7eff6f2c26c57636b0
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2025-09-08 14:13:11 -0400

    denis: unrestrict students who took an oopsie
    Fixes: #205
    
    As the final subs are processed, for any student who used their oopsie on
    the assignment, remove their restriction so they can see the emails.

--- denis/final.py
+++ denis/final.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 import sys
+import os
 
 import utilities
+import orbit.db
 
 assignment = sys.argv[1]
 
 usernames_to_subs = utilities.user_to_sub(assignment, 'final')
+
+for oopsie in orbit.db.Oopsie.select().where(orbit.db.Oopsie.assignment == assignment):
+    if usernames_to_subs[oopsie.user] is not None:
+        os.system(f'restrict_access /var/lib/email/journal/journal -a {oopsie.user}')
 
 utilities.release_subs([sub.submission_id for sub in usernames_to_subs.values() if sub])
 
