underground-software/singularity

commit cb688b43dd362967c649514b8eb06ccb4bad8de1

feat(container): add neovim to the container file

orbit/radius.py
@@ -831,6 +831,7 @@ RUN dnf -y update && \
 		dwarves \
 		git-email \
 		vim \
+        neovim \
 		nano \
 		{nano_default_editor} \
 		mutt \

commit 27058e901f2dd80f65e8ce8ff1c39ba38ed55f56

fixes whitespace (spaces to tabs)
Fixes: cb688b4 ("feat(container): add neovim to the container file")
Signed-off-by: Matt Myers <nt3058@gmail.com>

orbit/radius.py
@@ -831,7 +831,7 @@ RUN dnf -y update && \
 		dwarves \
 		git-email \
 		vim \
-        neovim \
+		neovim \
 		nano \
 		{nano_default_editor} \
 		mutt \