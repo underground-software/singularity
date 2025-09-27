underground-software/singularity

commit 88654d5907a1f641a3c69ce7a229992b2cc53692

denis: introduce dummy assignments whose deadlines are manually triggered
dummy assignments can be created using the new `dummy` option to configure
initially all deadlines are set in the far future so that testing code can
make submissions request oopsies etc without racing against the clock

when the relevant scenario has been staged, the `trigger` option can be
passed to configure specifying an assignment and component name and the
due date will be set to the current moment and the corresponding event
will be run synchronously.

denis/start.py
@@ -7,6 +7,8 @@ import sys
 
 import db
 
+from configure import far_future
+
 
 def spawn_waiter(timestamp, name, script):
     return subprocess.Popen(['/usr/local/bin/run-at', timestamp, script, name])
@@ -16,14 +18,49 @@ def in_the_future(ts):
     return datetime.datetime.now() < datetime.datetime.fromtimestamp(ts)
 
 
+def handle_trigger(info):
+    if info.si_code != -1:
+        print('spurious sigrtmin without queuing info!', file=sys.stderr)
+        return False
+    assignment_id, component_id = divmod(info.si_status, 3)
+    if not (asn := db.Assignment.get_or_none(db.Assignment.id == assignment_id)):
+        print(f'trigger for non existant assignment id {assignment_id}!', file=sys.stderr)
+        return
+    match component_id:
+        case 0:
+            component = 'initial submission'
+            attr = 'initial_due_date'
+            program = './initial.py'
+        case 1:
+            component = 'peer review'
+            attr = 'peer_review_due_date'
+            program = './peer_review.py'
+        case 2:
+            component = 'final submission'
+            attr = 'final_due_date'
+            program = './final.py'
+        case _:
+            print(f'invalid component id {component_id}!', file=sys.stderr)
+            return
+    if not in_the_future(getattr(asn, attr)):
+        print(f'{component} for {asn.name} already passed!', file=sys.stderr)
+        return
+
+    # update relevant deadline to current time so that dashboard etc behaves as expected
+    setattr(asn, attr, int(datetime.datetime.now().timestamp()))
+    asn.save()
+    subprocess.Popen([program, asn.name]).wait()
+
+
 def main():
-    # neither of these handlers should actually run,
+    # this handler function should never actually run,
     # but because we are pid 1 in container we need
-    # to register handlers or they wont be delivered
+    # to register handlers or signals wont be delivered
     def signal_handler(*_):
         assert False
     signal.signal(signal.SIGTERM, signal_handler)
     signal.signal(signal.SIGUSR1, signal_handler)
+    signal.signal(signal.SIGRTMIN, signal_handler)
 
     again = True
     while again:
@@ -34,26 +71,40 @@ def main():
             peer_review = assignment.peer_review_due_date
             final = assignment.final_due_date
 
-            if in_the_future(initial):
+            if initial == far_future:
+                pass
+            elif in_the_future(initial):
                 procs.append(spawn_waiter(str(initial), name, './initial.py'))
             else:
                 print(f'skipping initial for {name}', file=sys.stderr)
 
-            if in_the_future(peer_review):
+            if peer_review == far_future:
+                pass
+            elif in_the_future(peer_review):
                 procs.append(spawn_waiter(str(peer_review), name, './peer_review.py'))
             else:
                 print(f'skipping peer review for {name}', file=sys.stderr)
 
-            if in_the_future(final):
+            if final == far_future:
+                pass
+            elif in_the_future(final):
                 procs.append(spawn_waiter(str(final), name, './final.py'))
             else:
                 print(f'skipping final for {name}', file=sys.stderr)
 
-        # send SIGUSR1 to reload with new due dates, SIGTERM to exit
-        if signal.SIGUSR1 == signal.sigwait([signal.SIGUSR1, signal.SIGTERM]):
-            print('reloading', file=sys.stderr)
-        else:
-            again = False
+            while True:
+                # send SIGUSR1 to reload with new due dates, SIGTERM to exit, SIGRTMIN to trigger a deadline
+                info = signal.sigwaitinfo([signal.SIGUSR1, signal.SIGTERM, signal.SIGRTMIN])
+                match info.si_signo:
+                    case signal.SIGUSR1:
+                        print('reloading', file=sys.stderr)
+                    case signal.SIGTERM:
+                        again = False
+                    case signal.SIGRTMIN:
+                        handle_trigger(info)
+                        # no need to reload
+                        continue
+                break
 
         for proc in procs:
             proc.terminate()

denis/configure.py
@@ -5,6 +5,9 @@ from argparse import ArgumentParser as ap
 
 import db
 
+#  FIXME: if you are reading this in the year 9999...
+far_future = 253401417420
+
 
 def main():
     parser = ap(prog='configure', description='Configure assignments')
@@ -40,6 +43,9 @@ def main():
     add_peer_review(create_parser)
     add_final(create_parser)
 
+    dummy_parser = command_parsers.add_parser('dummy')
+    add_assignment(dummy_parser)
+
     alter_parser = command_parsers.add_parser('alter')
     add_assignment(alter_parser)
     add_initial(alter_parser, required=False)
@@ -57,6 +63,10 @@ def main():
 
     command_parsers.add_parser('reload')
 
+    trigger_parser = command_parsers.add_parser('trigger')
+    add_assignment(trigger_parser)
+    trigger_parser.add_argument('-c', '--component', dest='component', required=True)
+
     # Dictionary containing the desired command and all flags with their values
     kwargs = vars(parser.parse_args())
     # Subparsers store their name in the destination `'command'`
@@ -73,6 +83,16 @@ def create(assignment, initial, peer_review, final):
                              initial_due_date=initial,
                              peer_review_due_date=peer_review,
                              final_due_date=final)
+    except db.peewee.IntegrityError:
+        print('cannot create assignment with duplicate name')
+
+
+def dummy(assignment):
+    try:
+        db.Assignment.create(name=assignment,
+                             initial_due_date=far_future,
+                             peer_review_due_date=far_future,
+                             final_due_date=far_future)
     except db.peewee.IntegrityError:
         print('cannot create assignment with duplicate name')
 
@@ -119,6 +139,32 @@ def reload():
     import os
     import signal
     os.kill(1, signal.SIGUSR1)
+
+
+def trigger(assignment, component):
+    import signal
+    import ctypes
+    libc = ctypes.CDLL(None)
+    sigqueue = libc.sigqueue
+    sigqueue.restype = ctypes.c_int
+    sigqueue.argtypes = (ctypes.c_int, ctypes.c_int, ctypes.c_int)
+
+    if not (asn := db.Assignment.get_or_none(db.Assignment.name == assignment)):
+        print(f'no such assignment {assignment}')
+        return
+    match component:
+        case 'initial':
+            component_id = 0
+        case 'peer':
+            component_id = 1
+        case 'final':
+            component_id = 2
+        case _:
+            print(f'no such component {component}')
+            return
+
+    if err := sigqueue(1, signal.SIGRTMIN, asn.id * 3 + component_id):
+        print(f'sigqueue error: {err}')
 
 
 if __name__ == '__main__':

commit 02f0754d0d3a8ed5fe5d74838b2019506582c9bf

denis: start: fix accidentally nested loops
as part of the commit refactoring the signal waiting logic to introduce
dummy assignments I accidentally indented the code on click too far and
it ended up inside of the loop over each assignment which is completely
broken. Unironically owned by semantic whitespace, C would have fixed this

Fixes: 88654d5 ("denis: introduce dummy assignments whose deadlines are manually triggered")

denis/start.py
@@ -92,19 +92,19 @@ def main():
             else:
                 print(f'skipping final for {name}', file=sys.stderr)
 
-            while True:
-                # send SIGUSR1 to reload with new due dates, SIGTERM to exit, SIGRTMIN to trigger a deadline
-                info = signal.sigwaitinfo([signal.SIGUSR1, signal.SIGTERM, signal.SIGRTMIN])
-                match info.si_signo:
-                    case signal.SIGUSR1:
-                        print('reloading', file=sys.stderr)
-                    case signal.SIGTERM:
-                        again = False
-                    case signal.SIGRTMIN:
-                        handle_trigger(info)
-                        # no need to reload
-                        continue
-                break
+        while True:
+            # send SIGUSR1 to reload with new due dates, SIGTERM to exit, SIGRTMIN to trigger a deadline
+            info = signal.sigwaitinfo([signal.SIGUSR1, signal.SIGTERM, signal.SIGRTMIN])
+            match info.si_signo:
+                case signal.SIGUSR1:
+                    print('reloading', file=sys.stderr)
+                case signal.SIGTERM:
+                    again = False
+                case signal.SIGRTMIN:
+                    handle_trigger(info)
+                    # no need to reload
+                    continue
+            break
 
         for proc in procs:
             proc.terminate()