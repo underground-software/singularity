b4a876d0216ac10f845333a52d3679f9c894277e
Joel Savitz <joel@underground.software>
2026-09-16 13:14:31 -0400

    test: ensure plaintext podman-compose logs output
    podman-compose 1.6.0 introduced formatted prefixes and color options as the
    default output for the `podman-compose logs` command [0].
    
    We use sed commands to scan for particular plaintext strings in the logs
    output to verify container progress to a certain checkmark, so modify
    all these log output invokations with the flags necessary to ensure
    plaintext output as before.
    
    [0] https://github.com/containers/podman-compose/releases/tag/v1.6.0
    
    Signed-off-by: Joel Savitz <joel@underground.software>

--- start.sh
+++ start.sh
@@ -14,11 +14,11 @@ podman-compose up -d
 trap 'podman-compose down -v' EXIT
 
 # wait until synapse is done initializing
-podman-compose logs -f submatrix 2>&1 | sed '/Synapse now listening on TCP port 8008/ q'
+podman-compose logs --no-log-prefix --no-color -f submatrix 2>&1 | sed '/Synapse now listening on TCP port 8008/ q'
 ./test.sh
 podman-compose down -v
 podman-compose up -d
-podman-compose logs -f submatrix 2>&1 | sed '/Synapse now listening on TCP port 8008/ q'
+podman-compose logs --no-log-prefix --no-color -f submatrix 2>&1 | sed '/Synapse now listening on TCP port 8008/ q'
 ./dev_sockets.sh &
 ./test-sub1.sh
 podman-compose down -v
